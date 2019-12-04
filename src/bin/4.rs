01832de16c409bc5be5d825f68d8ded9f91c6aae
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2019-12-04 15:09:11 +0100

    Solution to 4

---
+++ src/bin/4.rs
@@ -0,0 +1,16 @@
+
+fn main() {
+  let answer = (206938..679128).filter(|n| {
+    let digits = n.to_string()
+      .chars()
+      .map(|c| c as u8 - b'0')
+      .collect::<Vec<_>>();
+    let mut repeating = false;
+    for i in 1..digits.len() {
+      if digits[i] < digits[i-1] { return false; }
+      if digits[i] == digits[i-1] { repeating = true; }
+    }
+    repeating
+  }).count();
+  println!("{}", answer);
+}
