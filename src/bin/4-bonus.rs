686492b0ff63e1edebd7431d4c31ef6b11bae4d2
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2019-12-04 15:36:30 +0100

    4 bonus

--- src/bin/4-bonus.rs
+++ src/bin/4-bonus.rs
@@ -1,3 +1,4 @@
+use itertools::Itertools;
 
 fn main() {
   let answer = (206938..679128)
@@ -6,12 +7,13 @@ fn main() {
         .chars()
         .map(|c| c as u8 - b'0')
         .collect::<Vec<_>>();
-      let mut repeating = false;
       for i in 1..digits.len() {
         if digits[i] < digits[i-1] { return false; }
-        if digits[i] == digits[i-1] { repeating = true; }
       }
-      repeating
+      digits.iter()
+        .group_by(|d| *d)
+        .into_iter()
+        .any(|(_,x)| x.count() == 2)
     })
     .count();
   println!("{}", answer);
