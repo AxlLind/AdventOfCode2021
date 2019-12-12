e9ec8781a6a2ffd30bcb6821da0c5941c9f4e6e2
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2019-12-12 16:40:21 +0100

    Clean up 12

--- src/bin/12-bonus.rs
+++ src/bin/12-bonus.rs
@@ -11,35 +11,32 @@ fn cmp_axis(a: i64, b: i64) -> i64 {
   }
 }
 
-fn step(moons: &mut [(i64,i64);4]) {
-  for i in 0..4 {
-    for j in (i+1)..4 {
-      let d = cmp_axis(moons[i].0, moons[j].0);
-      moons[i].1 += d;
-      moons[j].1 -= d;
-    }
-    moons[i].0 += moons[i].1;
-  }
-}
-
-fn simulate_one_axis(init: [i64;4]) -> usize {
-  let init = [
-    (init[0],0),
-    (init[1],0),
-    (init[2],0),
-    (init[3],0)
+fn simulate_one_axis(positions: [i64;4]) -> usize {
+  let mut moons = [
+    (positions[0],0),
+    (positions[1],0),
+    (positions[2],0),
+    (positions[3],0),
   ];
-  let mut moons = init.clone();
-  1 + iter::repeat(()).take_while(|_| {
-    step(&mut moons);
+  let init = moons.clone();
+  let steps = iter::repeat(()).take_while(|_| {
+    for i in 0..4 {
+      for j in (i+1)..4 {
+        let d = cmp_axis(moons[i].0, moons[j].0);
+        moons[i].1 += d;
+        moons[j].1 -= d;
+      }
+      moons[i].0 += moons[i].1;
+    }
     moons != init
-  }).count()
+  }).count();
+  steps + 1
 }
 
 fn main() {
   let now = Instant::now();
-  let x = simulate_one_axis([-10, 5, 3, 1]);
-  let y = simulate_one_axis([-10, 5, 8, 3]);
+  let x = simulate_one_axis([-10,  5,   3,  1]);
+  let y = simulate_one_axis([-10,  5,   8,  3]);
   let z = simulate_one_axis([-13, -9, -16, -3]);
   let answer = lcm(x,lcm(y,z));
   println!("{}", answer);
--- src/bin/12.rs
+++ src/bin/12.rs
@@ -1,21 +1,17 @@
 use std::time::Instant;
 use std::cmp::Ordering;
 
-#[derive(Default, Debug)]
+#[derive(Default)]
 struct Vec3 { x:i64, y:i64, z:i64 }
 
-#[derive(Debug)]
 struct Moon { pos: Vec3, v: Vec3 }
 
 impl Moon {
   fn from_pos(x: i64, y: i64, z: i64) -> Self {
-    Self {
-      pos: Vec3{ x,y,z },
-      v: Vec3::default(),
-    }
+    Self { pos: Vec3{ x,y,z }, v: Vec3::default() }
   }
 
-  fn get_energy(&self) -> i64 {
+  fn energy(&self) -> i64 {
     let pot = self.pos.x.abs() + self.pos.y.abs() + self.pos.z.abs();
     let kin = self.v.x.abs() + self.v.y.abs() + self.v.z.abs();
     pot * kin
@@ -33,40 +29,30 @@ fn cmp_axis(a: i64, b: i64) -> i64 {
 fn main() {
   let now = Instant::now();
   let mut moons = [
-    // Moon::from_pos(-10, -10, -13),
-    // Moon::from_pos(5, 5, -9),
-    // Moon::from_pos(3, 8, -16),
-    // Moon::from_pos(1, 3, -3),
-    Moon::from_pos(-1,0,2),
-    Moon::from_pos(2,-10,-7),
-    Moon::from_pos(4,-8,8),
-    Moon::from_pos(3,5,-1),
+    Moon::from_pos(-10, -10, -13),
+    Moon::from_pos(5, 5, -9),
+    Moon::from_pos(3, 8, -16),
+    Moon::from_pos(1, 3, -3),
   ];
-  for _ in 0..100 {
-    // calculate velocities
-    let mut velocities = vec![];
-    for m1 in &moons {
-      let mut v = Vec3::default();
-      for m2 in &moons {
-        v.x += cmp_axis(m1.pos.x, m2.pos.x);
-        v.y += cmp_axis(m1.pos.y, m2.pos.y);
-        v.z += cmp_axis(m1.pos.z, m2.pos.z);
+  for _ in 0..1000 {
+    for i in 0..4 {
+      for j in (i+1)..4 {
+        let dx = cmp_axis(moons[i].pos.x, moons[j].pos.x);
+        moons[i].v.x += dx;
+        moons[j].v.x -= dx;
+        let dy = cmp_axis(moons[i].pos.y, moons[j].pos.y);
+        moons[i].v.y += dy;
+        moons[j].v.y -= dy;
+        let dz = cmp_axis(moons[i].pos.z, moons[j].pos.z);
+        moons[i].v.z += dz;
+        moons[j].v.z -= dz;
       }
-      velocities.push(v);
-    }
-
-    // update velocities and positions
-    for (i,v) in velocities.iter().enumerate() {
-      moons[i].v.x += v.x;
-      moons[i].v.y += v.y;
-      moons[i].v.z += v.z;
       moons[i].pos.x += moons[i].v.x;
       moons[i].pos.y += moons[i].v.y;
       moons[i].pos.z += moons[i].v.z;
     }
   }
-  let answer = moons.iter().map(|m| m.get_energy()).sum::<i64>();
-  println!("{:?}", moons);
+  let answer = moons.iter().map(|m| m.energy()).sum::<i64>();
   println!("{}", answer);
   println!("Time: {}ms", now.elapsed().as_millis());
 }
