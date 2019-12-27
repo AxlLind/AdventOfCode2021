6e09649aa9c880fb72896b1b7feaaef709acebe0
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2019-12-27 14:41:21 +0100

    4: optimize to_digits

--- src/bin/4.rs
+++ src/bin/4.rs
@@ -1,35 +1,35 @@
 use std::time::Instant;
 use itertools::Itertools;
 
+fn to_digits(n: usize) -> [usize; 6] {
+  [
+    (n / 100000) % 10,
+    (n / 10000) % 10,
+    (n / 1000) % 10,
+    (n / 100) % 10,
+    (n / 10) % 10,
+    (n / 1) % 10,
+  ]
+}
+
 fn part_one() -> usize {
-  (206938..679128).filter(|n| {
-    let digits = n.to_string()
-      .chars()
-      .map(|c| c as u8 - b'0')
-      .collect::<Vec<_>>();
-    let mut repeating = false;
-    for i in 1..digits.len() {
-      if digits[i] < digits[i-1] { return false; }
-      if digits[i] == digits[i-1] { repeating = true; }
-    }
-    repeating
-  }).count()
+  (206938..679128)
+    .map(to_digits)
+    .filter(|&digits| (1..digits.len()).all(|i| digits[i-1] <= digits[i]))
+    .filter(|&digits| (1..digits.len()).any(|i| digits[i-1] == digits[i]))
+    .count()
 }
 
 fn part_two() -> usize {
-  (206938..679128).filter(|n| {
-    let digits = n.to_string()
-      .chars()
-      .map(|c| c as u8 - b'0')
-      .collect::<Vec<_>>();
-    if (1..digits.len()).any(|i| digits[i] < digits[i-1]) {
-      return false;
-    }
-    digits.iter()
+  (206938..679128)
+    .map(to_digits)
+    .filter(|&digits| (1..digits.len()).all(|i| digits[i-1] <= digits[i]))
+    .filter(|&digits| digits.iter()
       .group_by(|d| *d)
       .into_iter()
       .any(|(_,x)| x.count() == 2)
-  }).count()
+    )
+    .count()
 }
 
 fn main() {
