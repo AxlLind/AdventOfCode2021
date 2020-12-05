578751d8091ad78d7c31801c6540c1158bfe8897
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2020-12-05 18:05:10 +0100

    Add template code in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::time::Instant;
+
 fn main() {
-    println!("Hello, Advent of Code 2020!");
+  let now = Instant::now();
+  // this is a template, add solution here
+  println!("Part one: {}", 0);
+  println!("Part two: {}", 0);
+  println!("Time: {}ms", now.elapsed().as_millis());
 }
