85fc2bb2d07a50f709ffe971bfead004ec623862
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2020-12-15 09:19:06 +0100

    15: slightly faster by using u32

--- src/bin/15.rs
+++ src/bin/15.rs
@@ -1,9 +1,9 @@
 use std::collections::*;
 
-fn solve(target: usize) -> usize {
+fn solve(target: u32) -> u32 {
   let mut seen = [9,19,1,6,0,5].iter()
     .enumerate()
-    .map(|(i,&e)| (e, i+1))
+    .map(|(i,&e)| (e, i as u32 + 1))
     .collect::<HashMap<_,_>>();
   (7..target).fold(4, |last, i| i - seen.insert(last, i).unwrap_or(i))
 }
