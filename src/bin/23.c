1f03a22209487e7c86c5765cbfa5f882f35e100a
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2020-12-23 11:40:49 +0100

    23: 30% speed up by computing value using pointer arithmetic

--- src/bin/23.c
+++ src/bin/23.c
@@ -2,21 +2,17 @@
 #include <stdio.h>
 #include <time.h>
 
+#define VAL(n) (n - list)
 int INPUT[] = {4,6,3,5,2,8,1,7,9};
 
-typedef struct Node {
-  int val;
-  struct Node *next;
-} Node;
+typedef struct Node { struct Node *next; } Node;
 
 Node* build_list(int size) {
   Node *list = malloc(sizeof(Node) * size);
   Node *curr = list + INPUT[0];
-  curr->val = INPUT[0];
   for (int i = 1; i < size; curr = curr->next, ++i) {
     int val = i < 9 ? INPUT[i] : i + 1;
     curr->next = list + val;
-    curr->next->val = val;
   }
   curr->next = list + INPUT[0];
   return list;
@@ -27,8 +23,8 @@ Node* simulate(int size, int rounds) {
   Node *curr = list + INPUT[0];
   for (int i = 0; i < rounds; curr = curr->next, ++i) {
     Node *a = curr->next, *b = a->next, *c = b->next;
-    int t = (curr->val == 1 ? size : curr->val - 1);
-    while (t == a->val || t == b->val || t == c->val)
+    int t = (VAL(curr) == 1 ? size : VAL(curr) - 1);
+    while (t == VAL(a) || t == VAL(b) || t == VAL(c))
       t = (t == 1 ? size : t - 1);
     Node *dest = list + t;
 
@@ -36,20 +32,20 @@ Node* simulate(int size, int rounds) {
     c->next = dest->next;
     dest->next = a;
   }
-  return list + 1;
+  return list;
 }
 
 int part_one(void) {
-  Node *one = simulate(9, 100);
+  Node *list = simulate(9, 100);
   int ans = 0;
-  for (Node *n = one->next; n->val != 1; n = n->next)
-    ans = ans * 10 + n->val;
+  for (Node *n = list[1].next; VAL(n) != 1; n = n->next)
+    ans = ans * 10 + VAL(n);
   return ans;
 }
 
 long part_two(void) {
-  Node *one = simulate(1000000, 10000000);
-  return one->next->val * one->next->next->val;
+  Node *list = simulate(1000000, 10000000);
+  return VAL(list[1].next) * VAL(list[1].next->next);
 }
 
 int main() {
