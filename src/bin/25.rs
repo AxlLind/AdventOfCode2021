e7f0d40deafea6aadb1594a4f8729b81d3d15e31
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2020-12-25 06:27:34 +0100

    25: final day

---
+++ src/bin/25.rs
@@ -0,0 +1,13 @@
+const A: usize = 13316116;
+const B: usize = 13651422;
+
+aoc2020::main! {
+  let (mut x, mut loop_size) = (1,0);
+  while x != A && x != B {
+    x = x * 7 % 20201227;
+    loop_size += 1;
+  }
+  let a = if x == A {B} else {A};
+  let key = (0..loop_size).fold(1, |x,_| x * a % 20201227);
+  (key, "🎄")
+}
