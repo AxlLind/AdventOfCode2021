bef4614bb4f01e84f3c0a27b6729cca21d25f7e2
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2021-01-25 23:01:37 +0100

    17: simplify powerset fn

--- src/17.clj
+++ src/17.clj
@@ -1,12 +1,8 @@
 (def input '(43 3 4 10 21 44 4 6 47 41 34 17 17 44 36 31 46 9 27 38))
 
 (defn powerset [base-set]
-  (->> base-set
-       (reduce
-         (fn [xs x]
-           (concat xs
-                   (map #(cons x %) xs)))
-         [()])))
+  (defn sets [xs x] (concat xs (map #(cons x %) xs)))
+  (reduce sets [()] base-set))
 
 (let [valid (->> input powerset (filter #(= 150 (reduce + %))))]
   (->> valid count (println "Part one:"))
