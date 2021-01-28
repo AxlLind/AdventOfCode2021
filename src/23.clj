3314077500edf7243c0629f86e43024c64660cb9
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2021-01-28 08:07:28 +0100

    23: rename variables for clarity

--- src/23.clj
+++ src/23.clj
@@ -1,13 +1,13 @@
 (def input '[(:jio :a 19) (:inc :a) (:tpl :a) (:inc :a) (:tpl :a) (:inc :a) (:tpl :a) (:tpl :a) (:inc :a) (:inc :a) (:tpl :a) (:tpl :a) (:inc :a) (:inc :a) (:tpl :a) (:inc :a) (:inc :a) (:tpl :a) (:jmp 23) (:tpl :a) (:tpl :a) (:inc :a) (:inc :a) (:tpl :a) (:inc :a) (:inc :a) (:tpl :a) (:inc :a) (:tpl :a) (:inc :a) (:tpl :a) (:inc :a) (:tpl :a) (:inc :a) (:inc :a) (:tpl :a) (:inc :a) (:inc :a) (:tpl :a) (:tpl :a) (:inc :a) (:jio :a 8) (:inc :b) (:jie :a 4) (:tpl :a) (:inc :a) (:jmp 2) (:hlf :a) (:jmp -7)])
 
-(defn run-inst [ip regs [code op1 op2]]
+(defn run-inst [ip regs [code x y]]
   (case code
-    :hlf [(inc ip) (update regs op1 #(/ % 2))]
-    :tpl [(inc ip) (update regs op1 #(* % 3))]
-    :inc [(inc ip) (update regs op1 inc)]
-    :jmp [(+ ip op1) regs]
-    :jie [(+ ip (if (even? (regs op1)) op2 1)) regs]
-    :jio [(+ ip (if (= 1   (regs op1)) op2 1)) regs]))
+    :hlf [(+ ip 1) (update regs x #(/ % 2))]
+    :tpl [(+ ip 1) (update regs x #(* % 3))]
+    :inc [(+ ip 1) (update regs x inc)]
+    :jmp [(+ ip x) regs]
+    :jie [(+ ip (if (even? (regs x)) y 1)) regs]
+    :jio [(+ ip (if (= 1   (regs x)) y 1)) regs]))
 
 (defn run [insts [ip regs]]
   (let [i (get insts ip)]
