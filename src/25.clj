dcad3d2ad9e06b743d2a9f04811769ecb74c5e58
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2021-01-28 07:58:56 +0100

    25: clean up

--- src/25.clj
+++ src/25.clj
@@ -1,12 +1,10 @@
 (def input '(3010 3019))
 
-(defn walk-diagonal [r c pre]
+(defn walk-diagonal [r c v]
   (if (= [r c] input)
-    pre
-  (let [new (mod (* pre 252533) 33554393)]
-  (if (= r 1)
-    (recur (inc c) 1 new)
-    (recur (dec r) (inc c) new)))))
+    v
+    (let [[r c] (if (= r 1) [(inc c) 1] [(dec r) (inc c)])]
+      (recur r c (mod (* v 252533) 33554393)))))
 
 (println "Part one:" (walk-diagonal 1 1 20151125))
 (println "Part two:" "🎄")
