88eb15e19e61fb98994e3e856246bbd5e9b6e920
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2021-07-22 10:30:18 +0200

    18: slightly simplify parsing

--- src/day18.ml
+++ src/day18.ml
@@ -6,23 +6,23 @@ let rec next_row res i s =
   else
     let a = if i = 0 then '.' else s.[i-1] in
     let c = if i + 1 = String.length s then '.' else s.[i+1] in
-    let tile = match [a;s.[i];c] with
-    | ['^'; '^'; '.'] -> "^"
-    | ['.'; '^'; '^'] -> "^"
-    | ['.'; '.'; '^'] -> "^"
-    | ['^'; '.'; '.'] -> "^"
+    let tile = match (a,s.[i],c) with
+    | '^','^','.' -> "^"
+    | '.','^','^' -> "^"
+    | '.','.','^' -> "^"
+    | '^','.','.' -> "^"
     | _ -> "." in
     next_row (tile::res) (i+1) s
 
 let num_safe s =
   s |> String.to_seq |> Seq.filter ((=) '.') |> List.of_seq |> List.length
 
-let rec safe_tiles c s left =
-  if left = 0 then c else safe_tiles (s |> num_safe |> (+) c) (next_row [] 0 s) (left-1)
+let rec safe_tiles s c left =
+  if left = 0 then c else safe_tiles (next_row [] 0 s) (num_safe s + c) (left-1)
 
 let main () =
-  let part1 = 40     |> safe_tiles 0 input |> string_of_int in
-  let part2 = 400000 |> safe_tiles 0 input |> string_of_int in
-  (part1,part2)
+  let part1 = safe_tiles input 0 40 in
+  let part2 = safe_tiles input 0 400000 in
+  (string_of_int part1, string_of_int part2)
 
 let () = Aoc.timer main
