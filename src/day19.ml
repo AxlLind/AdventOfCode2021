00102f5c239ef2e4a815300429dff3ef626f7683
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2021-07-22 10:31:54 +0200

    19: change main

--- src/day19.ml
+++ src/day19.ml
@@ -1,15 +1,14 @@
 let input = 3012210
 
 let rec josephus i =
-  if i < 3 then 1
-  else josephus (i/2) * 2 + ((i mod 2) * 2 - 1)
+  if i < 3 then 1 else josephus (i/2) * 2 + ((i mod 2) * 2 - 1)
 
 let rec find_pow_three i =
   if i * 3 > input then i else find_pow_three (i*3)
 
 let main () =
-  let part1 = input |> josephus |> string_of_int in
-  let part2 = input - (find_pow_three 1) |> string_of_int in
-  (part1, part2)
+  let part1 = josephus input in
+  let part2 = input - (find_pow_three 1) in
+  (string_of_int part1, string_of_int part2)
 
 let () = Aoc.timer main
