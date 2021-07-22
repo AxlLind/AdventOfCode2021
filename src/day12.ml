0662ff74a7da1a838db34dfe04f13502ec9d69e2
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2021-07-22 10:02:32 +0200

    12: cleaner parsing code

--- src/day12.ml
+++ src/day12.ml
@@ -1,37 +1,35 @@
 let input = "cpy 1 a\ncpy 1 b\ncpy 26 d\njnz c 2\njnz 1 5\ncpy 7 c\ninc d\ndec c\njnz c -2\ncpy a c\ninc a\ndec b\njnz b -2\ncpy c b\ndec d\njnz d -6\ncpy 19 c\ncpy 11 d\ninc a\ndec d\njnz d -2\ndec c\njnz c -5"
 
 type opcode =
-  | Cpy of bool * int * int
+  | Cpy of (bool * int) * int
+  | Jnz of (bool * int) * (bool * int)
   | Inc of int
   | Dec of int
-  | Jnz of int * int
 
 let parse_line s =
-  let reg_to_int s = match s.[0] with
-  | 'a' -> 0
-  | 'b' -> 1
-  | 'c' -> 2
-  | 'd' -> 3
-  | _ -> failwith ("unreachable " ^ s) in
+  let parse_reg_or_value s = match s.[0] with
+  | 'a' -> (true, 0)
+  | 'b' -> (true, 1)
+  | 'c' -> (true, 2)
+  | 'd' -> (true, 3)
+  | _   -> (false, int_of_string s) in
   match s |> String.split_on_char ' ' with
-  | [op;a;b] ->
-    let (reg,src) =
-      if String.contains "abcd" a.[0] then (true, reg_to_int a)
-      else (false, int_of_string a) in
-    if op = "cpy" then
-      Cpy(reg, src, reg_to_int b)
-    else
-      Jnz(src, int_of_string b)
-  | [op;a] ->
-    let reg = reg_to_int a in
-    if op = "inc" then Inc(reg) else Dec(reg)
+  | ["cpy";a;b] -> Cpy(parse_reg_or_value a, b |> parse_reg_or_value |> snd)
+  | ["jnz";a;b] -> Jnz(parse_reg_or_value a, parse_reg_or_value b)
+  | ["inc";a] -> Inc(a |> parse_reg_or_value |> snd)
+  | ["dec";a] -> Dec(a |> parse_reg_or_value |> snd)
   | _ -> failwith "unreachable"
 
-let exec_inst regs inst = match inst with
-  | Cpy (reg_src, src, dst) -> regs.(dst) <- if reg_src then regs.(src) else src; 1
-  | Inc (dst) -> regs.(dst) <- regs.(dst) + 1; 1
-  | Dec (dst) -> regs.(dst) <- regs.(dst) - 1; 1
-  | Jnz (src, off) -> if regs.(src) = 0 then 1 else off
+let exec_inst regs inst =
+  let reg_val (is_reg, i) = if is_reg then regs.(i) else i in
+  let () = match inst with
+  | Cpy (src, dst) -> regs.(dst) <- reg_val src
+  | Inc dst -> regs.(dst) <- regs.(dst) + 1
+  | Dec dst -> regs.(dst) <- regs.(dst) - 1
+  | _ -> () in
+  match inst with
+  | Jnz (src, off) when reg_val src != 0 -> reg_val off
+  | _ -> 1
 
 let rec exec insts regs ip =
   if ip < List.length insts then
@@ -41,8 +39,8 @@ let rec exec insts regs ip =
 
 let main () =
   let insts = input |> Aoc.parse_lines parse_line in
-  let part1 = exec insts [|0;0;0;0|] 0 |> string_of_int in
-  let part2 = exec insts [|0;0;1;0|] 0 |> string_of_int in
-  (part1, part2)
+  let part1 = exec insts [|0;0;0;0|] 0 in
+  let part2 = exec insts [|0;0;1;0|] 0 in
+  (string_of_int part1, string_of_int part2)
 
 let () = Aoc.timer main
