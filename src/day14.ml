781f7c42209a92068caf374882123a2cd4a747cb
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2021-07-22 10:16:07 +0200

    14: simplify queue creation

--- src/day14.ml
+++ src/day14.ml
@@ -2,24 +2,23 @@ let input = "ihaygndm"
 
 let hash_num times i =
   let rec hash_impl left h =
-    if left > 0 then
-      h |> Digest.string |> Digest.to_hex |> hash_impl (left-1)
-    else h
+    if left = 0 then h
+    else h |> Digest.string |> Digest.to_hex |> hash_impl (left-1)
   in
   hash_impl times (input ^ (string_of_int i))
 
 let rec all_fives res i h =
-  if i + 4 < String.length h then
+  if i + 4 = String.length h then res
+  else
     let found = [1;2;3;4] |> List.for_all (fun j -> h.[i] = h.[i+j]) in
     let res = if found then (h.[i]::res) else res in
     all_fives res (i+1) h
-  else res
 
 let rec hash_triple i h =
-  if i < String.length h then
+  if i = String.length h then None
+  else
     let (a,b,c) = (h.[i-2], h.[i-1], h.[i]) in
     if a = b && b = c then Some a else hash_triple (i+1) h
-  else None
 
 let handle_hash_num times i =
   let h = hash_num times i in
@@ -27,12 +26,10 @@ let handle_hash_num times i =
   let fives = all_fives [] 0 h in
   (c, fives)
 
-let init_queue times =
-  let queue = Queue.create () in
-  for i = 0 to 999 do
-    queue |> Queue.add (handle_hash_num times i)
-  done;
-  queue
+let init_queue times = List.init 1000 (fun i -> i)
+  |> List.to_seq
+  |> Seq.map (handle_hash_num times)
+  |> Queue.of_seq
 
 let find_64th times =
   let q = init_queue times in
@@ -55,8 +52,8 @@ let find_64th times =
   find_impl [] 0
 
 let main () =
-  let part1 = 1    |> find_64th |> string_of_int in
-  let part2 = 2017 |> find_64th |> string_of_int in
-  (part1, part2)
+  let part1 = find_64th 1 in
+  let part2 = find_64th 2017 in
+  (string_of_int part1, string_of_int part2)
 
 let () = Aoc.timer main
