96fad04cca418976088272aa529b2fea9aac3b5f
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2021-07-29 11:36:55 +0200

    Use Aoc timer in 02,03

--- src/03.hs
+++ src/03.hs
@@ -1,44 +1,46 @@
+import Aoc
 import Data.Function
-import qualified Data.Map as M
+import qualified Data.Map as Map
 
-type PosMap = M.Map (Int,Int) Int
+type PosMap = Map.Map (Int,Int) Int
 type State = ((Int,Int), (Int,Int), PosMap)
 
-neighbours = [(1,-1),(1,0),(1,1),(0,-1),(0,1),(-1,-1),(-1,0),(-1,1)]
-initState = ((0,0), (1,0), (M.fromList [((0,0),1)]))
+initState = ((0,0), (1,0), (Map.fromList [((0,0),1)]))
 
 turn :: (Int,Int) -> (Int,Int)
 turn (1,0)  = (0,1)
 turn (-1,0) = (0,-1)
 turn (0,1)  = (-1,0)
 turn (0,-1) = (1,0)
-turn pos = pos
+turn dir = dir
 
 atCorner :: PosMap -> (Int,Int) -> Bool
-atCorner m (x,y) = [(x+1,y),(x-1,y),(x,y+1),(x,y-1)] & filter (\pos -> M.notMember pos m) & length & (==3)
+atCorner m (x,y) = neighbours & filter (\pos -> Map.notMember pos m) & length & (==3)
+  where neighbours = [(x+1,y),(x-1,y),(x,y+1),(x,y-1)]
 
 move :: Int -> State -> State
-move v (x,y) (dx,dy) m = (newPos, newDir, newMap)
+move v ((x,y),(dx,dy),m) = (newPos, newDir, newMap)
   where
-  newPos = (x+dx, y+dy)
-  newDir = if atCorner m newPos then turn (dx,dy) else (dx,dy)
-  newMap = M.insert newPos v m
+    newPos = (x+dx, y+dy)
+    newDir = if atCorner m newPos then turn (dx,dy) else (dx,dy)
+    newMap = Map.insert newPos v m
 
 findPosOf :: Int -> Int -> State -> (Int,Int)
 findPosOf goal prev state = if prev+1 == goal then newPos else findPosOf goal (prev+1) (newPos, newDir, newMap)
-  where
-  (newPos, newDir, newMap) = move (prev+1) state
+  where (newPos, newDir, newMap) = move (prev+1) state
 
 findPart1 :: Int -> Int
 findPart1 goal = (abs x) + (abs y)
   where (x,y) = findPosOf goal 1 initState
 
-findPart2 :: Int -> (Int,Int) -> (Int,Int) -> PosMap -> Int
-findPart2 goal ((x,y),(dx,dy),m) = if newVal > goal then newVal else findPart2 goal (newPos, newDir, newMap)
+findPart2 :: Int -> State -> Int
+findPart2 goal ((x,y),(dx,dy),m) = if newVal > goal then newVal else findPart2 goal newState
   where
-  newVal = neighbours & map (\(a,b) -> M.findWithDefault 0 (x+dx+a,y+dy+b) m) & sum
-  (newPos, newDir, newMap) = move newVal ((x,y),(dx,dy),m)
+    neighbours = [(1,-1),(1,0),(1,1),(0,-1),(0,1),(-1,-1),(-1,0),(-1,1)]
+    newVal = neighbours & map (\(a,b) -> Map.findWithDefault 0 (x+dx+a,y+dy+b) m) & sum
+    newState = move newVal ((x,y),(dx,dy),m)
 
-main = do
-  findPart1 289326 & show & ("Part 1: " ++) & putStrLn
-  findPart2 289326 initState & show & ("Part 2: " ++) & putStrLn
+solveParts :: Int -> (Int,Int)
+solveParts _ = (findPart1 289326, findPart2 289326 initState)
+
+main = Aoc.timer solveParts
