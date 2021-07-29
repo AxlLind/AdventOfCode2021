d26b7f817f73a78d57fcb56ad6ad4adcc2febff8
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2021-07-29 16:35:33 +0200

    06: better function name

--- src/06.hs
+++ src/06.hs
@@ -14,13 +14,13 @@ moveMaximum v = v // updates
     idx j = mod (i+j) (Vec.length v)
     updates = (i,0):[(idx j, (v ! idx j) + 1) | j <- [1..(v ! i)]]
 
-solvePart1 :: Map (Vector Int) Int -> Int -> Vector Int -> (Int -> Int -> Int) -> Int
-solvePart1 seen c v answerFn = case Map.lookup v seen of
+findRepeat :: Map (Vector Int) Int -> Int -> Vector Int -> (Int -> Int -> Int) -> Int
+findRepeat seen c v answerFn = case Map.lookup v seen of
   Just x -> answerFn x c
-  Nothing -> solvePart1 (Map.insert v c seen) (c+1) (moveMaximum v) answerFn
+  Nothing -> findRepeat (Map.insert v c seen) (c+1) (moveMaximum v) answerFn
 
 solveParts :: Int -> (Int,Int)
 solveParts _ = (solverFn (\_ y -> y), solverFn (\x y -> y - x))
-  where solverFn = words input & map read & Vec.fromList & solvePart1 Map.empty 0
+  where solverFn = words input & map read & Vec.fromList & findRepeat Map.empty 0
 
 main = Aoc.timer solveParts
