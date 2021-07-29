aeb509cf8be0ae02d940a8c564767caf472e0209
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2021-07-29 15:59:54 +0200

    Add a script to easily run a solution

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+# A simple script to compile and run a solution.
+set -e
+
+usage_exit() { printf "$1\nUsage: $0 01\n"; exit 1; }
+[ $# -eq 1       ] || usage_exit "Missing argument."
+[ -f ./src/$1.hs ] || usage_exit "File not found."
+
+ghc -O ./src/$1.hs > /dev/null
+./src/$1
