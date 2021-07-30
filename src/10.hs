f12a3c809ec34c11e5616b674ee96487ddc4f925
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2021-07-30 12:01:55 +0200

    Finish day 10

---
+++ src/10.hs
@@ -0,0 +1,44 @@
+import Aoc
+import Data.Function
+import Data.Char
+import Numeric (showHex)
+import Data.List.Split (chunksOf, splitOn)
+import Data.Bits (xor)
+
+input = "192,69,168,160,78,1,166,28,0,83,198,2,254,255,41,12"
+
+toHex :: Int -> String
+toHex i = (if i < 16 then "0" else "") ++ showHex i ""
+
+splitRange :: [Int] -> (Int,Int) -> ([Int],[Int],[Int])
+splitRange lst (i,len) = (a,b,c)
+  where
+    (a,tmp) = splitAt i lst
+    (b,c) = splitAt len tmp
+
+doOperation :: ([Int], Int, Int) -> Int -> ([Int], Int, Int)
+doOperation (lst, i, skip) len = (newList, (i+skip+len) `mod` lstLen, skip+1)
+  where
+    lstLen = length lst
+    (a,b,c) = splitRange (lst++lst) (i,len)
+    revList = a ++ reverse b ++ c
+    newList =
+      let spillover = i + len - lstLen in
+      let part = take spillover (drop lstLen revList) in
+      take lstLen (part ++ (drop spillover revList))
+
+knotHash :: [Int] -> String
+knotHash lengths = chunksOf 16 lst & map (toHex . foldl xor 0) & concat
+  where
+    (lst,_,_) = replicate 64 lengths & concat & foldl doOperation ([0..255],0,0)
+
+solveParts :: Int -> (Int,String)
+solveParts _ = (part1, part2)
+  where
+    inputPart1 = splitOn "," input & map read
+    inputPart2 = map ord input ++ [17, 31, 73, 47, 23]
+    (lst,_,_) = foldl doOperation ([0..255],0,0) inputPart1
+    part1 = (head lst) * (head (tail lst))
+    part2 = knotHash inputPart2
+
+main = Aoc.timer solveParts
