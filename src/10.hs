3827d7363f377dbfb5081a0d4d9ad1078bdd0217
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2021-08-03 15:47:00 +0200

    10: remove unnecessary helper fns

--- src/10.hs
+++ src/10.hs
@@ -7,37 +7,28 @@ import Data.Bits (xor)
 
 input = "192,69,168,160,78,1,166,28,0,83,198,2,254,255,41,12"
 
-toHex :: Int -> String
-toHex i = (if i < 0x10 then "0" else "") ++ showHex i ""
-
-splitRange :: [Int] -> (Int,Int) -> ([Int],[Int],[Int])
-splitRange lst (i,len) = (a,b,c)
+performKnot :: ([Int], Int, Int) -> Int -> ([Int], Int, Int)
+performKnot (lst, i, skip) len = (newList, (i+skip+len) `mod` 256, skip+1)
   where
     (a,tmp) = splitAt i lst
-    (b,c) = splitAt len tmp
-
-doOperation :: ([Int], Int, Int) -> Int -> ([Int], Int, Int)
-doOperation (lst, i, skip) len = (newList, (i+skip+len) `mod` 256, skip+1)
-  where
-    (a,b,c) = splitRange (lst++lst) (i,len)
+    (b,c) = splitAt len (tmp++lst)
     revList = a ++ reverse b ++ c
     newList =
       let spillover = i + len - 256 in
       let part = take spillover (drop 256 revList) in
-      take 256 (part ++ (drop spillover revList))
+      take 256 (part ++ drop spillover revList)
 
 knotHash :: String -> String
-knotHash s = chunksOf 16 lst & map (toHex . foldl xor 0) & concat
+knotHash s = chunksOf 16 lst & concatMap (toHex . foldl xor 0)
   where
     lengths = map ord s ++ [17, 31, 73, 47, 23]
-    (lst,_,_) = replicate 64 lengths & concat & foldl doOperation ([0..255],0,0)
+    (lst,_,_) = concatMap (const lengths) [1..64] & foldl performKnot ([0..255],0,0)
+    toHex i = (if i < 0x10 then "0" else "") ++ showHex i ""
 
 solveParts :: Int -> (Int,String)
-solveParts _ = (part1, part2)
+solveParts _ = (part1, knotHash input)
   where
-    inputPart1 = splitOn "," input & map read
-    (lst,_,_) = foldl doOperation ([0..255],0,0) inputPart1
-    part1 = (head lst) * (head (tail lst))
-    part2 = knotHash input
+    (lst,_,_) = splitOn "," input & map read & foldl performKnot ([0..255],0,0)
+    part1 = head lst * lst!!1
 
 main = Aoc.timer solveParts
