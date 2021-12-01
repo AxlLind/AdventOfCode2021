58232e56e5b4c3c20c883ea641a2916efe3b8acb
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2021-12-01 06:47:20 +0100

    Update to 2021 in fetch script

--- fetch.sh
+++ fetch.sh
@@ -15,6 +15,5 @@ if [ -z "$AOC_SESSION" ]; then
   exit 1
 fi
 
-URL="https://adventofcode.com/2020/day/$1/input"
+URL="https://adventofcode.com/2021/day/$1/input"
 curl $URL --cookie "session=$AOC_SESSION" -s | tee >(pbcopy)
-
