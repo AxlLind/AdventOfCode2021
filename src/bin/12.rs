4336af51e501c960643929df3da0f12462115450
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2021-12-12 08:22:13 +0100

    12: use bool instead of option node

--- src/bin/12.rs
+++ src/bin/12.rs
@@ -6,16 +6,16 @@ fn num_paths<'a>(
   graph: &HashMap<&'a str, Vec<&'a str>>,
   src: &'a str,
   path: &mut Vec<&'a str>,
-  mut seen_twice: Option<&'a str>
+  mut seen_twice: bool
 ) -> usize {
   if src == "end" {
     return 1;
   }
   if src.chars().all(|c| c.is_lowercase()) && path.contains(&src) {
-    if seen_twice.is_some() || src == "start" {
+    if seen_twice || src == "start" {
       return 0;
     }
-    seen_twice = Some(src);
+    seen_twice = true;
   }
   path.push(src);
   let ans = graph[src].iter()
@@ -32,7 +32,7 @@ aoc2021::main! {
     graph.entry(a).or_insert(Vec::new()).push(b);
     graph.entry(b).or_insert(Vec::new()).push(a);
   }
-  let p1 = num_paths(&graph, "start", &mut Vec::new(), Some(""));
-  let p2 = num_paths(&graph, "start", &mut Vec::new(), None);
+  let p1 = num_paths(&graph, "start", &mut Vec::new(), true);
+  let p2 = num_paths(&graph, "start", &mut Vec::new(), false);
   (p1,p2)
 }
