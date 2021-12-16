ba2d91ebc8e1828c4d7e77da937779e19bab05c3
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2021-12-16 07:36:32 +0100

    Finish day 16

---
+++ src/bin/16.rs
@@ -0,0 +1,123 @@
+
+static INPUT: &str = "2056FA18025A00A4F52AB13FAB6CDA779E1B2012DB003301006A35C7D882200C43289F07A5A192D200C1BC011969BA4A485E63D8FE4CC80480C00D500010F8991E23A8803104A3C425967260020E551DC01D98B5FEF33D5C044C0928053296CDAFCB8D4BDAA611F256DE7B945220080244BE59EE7D0A5D0E6545C0268A7126564732552F003194400B10031C00C002819C00B50034400A70039C009401A114009201500C00B00100D00354300254008200609000D39BB5868C01E9A649C5D9C4A8CC6016CC9B4229F3399629A0C3005E797A5040C016A00DD40010B8E508615000213112294749B8D67EC45F63A980233D8BCF1DC44FAC017914993D42C9000282CB9D4A776233B4BF361F2F9F6659CE5764EB9A3E9007ED3B7B6896C0159F9D1EE76B3FFEF4B8FCF3B88019316E51DA181802B400A8CFCC127E60935D7B10078C01F8B50B20E1803D1FA21C6F300661AC678946008C918E002A72A0F27D82DB802B239A63BAEEA9C6395D98A001A9234EA620026D1AE5CA60A900A4B335A4F815C01A800021B1AE2E4441006A0A47686AE01449CB5534929FF567B9587C6A214C6212ACBF53F9A8E7D3CFF0B136FD061401091719BC5330E5474000D887B24162013CC7EDDCDD8E5E77E53AF128B1276D0F980292DA0CD004A7798EEEC672A7A6008C953F8BD7F781ED00395317AF0726E3402100625F3D9CB18B546E2FC9C65D1C20020E4C36460392F7683004A77DB3DB00527B5A85E06F253442014A00010A8F9106108002190B61E4750004262BC7587E801674EB0CCF1025716A054AD47080467A00B864AD2D4B193E92B4B52C64F27BFB05200C165A38DDF8D5A009C9C2463030802879EB55AB8010396069C413005FC01098EDD0A63B742852402B74DF7FDFE8368037700043E2FC2C8CA00087C518990C0C015C00542726C13936392A4633D8F1802532E5801E84FDF34FCA1487D367EF9A7E50A43E90";
+
+#[derive(Debug, Clone)]
+enum Instruction {
+  Literal(u8, usize),
+  Operator(u8, u8, Vec<Instruction>),
+}
+
+fn decode_hex(s: &str) -> Vec<u8> {
+  let mut vec = Vec::new();
+  for c in s.chars() {
+    let bits = match c {
+      '0' => "0000",
+      '1' => "0001",
+      '2' => "0010",
+      '3' => "0011",
+      '4' => "0100",
+      '5' => "0101",
+      '6' => "0110",
+      '7' => "0111",
+      '8' => "1000",
+      '9' => "1001",
+      'A' => "1010",
+      'B' => "1011",
+      'C' => "1100",
+      'D' => "1101",
+      'E' => "1110",
+      'F' => "1111",
+      _ => unreachable!(),
+    };
+    vec.extend(bits.bytes().map(|b| b - b'0'));
+  }
+  vec
+}
+
+fn from_bytes(bytes: &[u8]) -> usize {
+  let mut x = 0;
+  for &b in bytes {
+    x = (x << 1) | b as usize
+  }
+  x
+}
+
+fn parse_instruction(b: &[u8], max_insts: usize) -> (usize,Vec<Instruction>) {
+  let mut i = 0;
+  let mut insts = Vec::new();
+  while i < b.len() && insts.len() < max_insts {
+    let version = from_bytes(&b[i..(i+3)]) as u8;
+    let type_id = from_bytes(&b[(i+3)..(i+6)]);
+    i += 6;
+    match type_id {
+      4 => {
+        // literal
+        let mut val = 0;
+        loop {
+          val <<= 4;
+          let x = from_bytes(&b[i..(i+5)]);
+          val += x & 0xf;
+          i += 5;
+          if x >> 4 == 0 {
+            break;
+          }
+        }
+        insts.push(Instruction::Literal(version, val));
+      }
+      id => {
+        // an operator
+        i += 1;
+        let new_insts = match b[i-1] {
+          0 => {
+            let nbits = from_bytes(&b[i..(i+15)]) as usize;
+            i += 15;
+            let (_, new_insts) = parse_instruction(&b[i..(i+nbits)], usize::MAX);
+            i += nbits;
+            new_insts
+          }
+          _ => {
+            let ninsts = from_bytes(&b[i..(i+11)]) as usize;
+            i += 11;
+            let (j, new_insts) = parse_instruction(&b[i..], ninsts);
+            i += j;
+            new_insts
+          }
+        };
+        insts.push(Instruction::Operator(version, id as u8, new_insts));
+      }
+    }
+  }
+  (i, insts)
+}
+
+fn part1(inst: &Instruction) -> usize {
+  match inst {
+    Instruction::Literal(version, _) => *version as usize,
+    Instruction::Operator(version, _, insts) =>
+     *version as usize + insts.iter().map(part1).sum::<usize>()
+  }
+}
+
+fn part2(inst: &Instruction) -> usize {
+  match inst {
+    Instruction::Literal(_, val) => *val as usize,
+    Instruction::Operator(_, id, insts) => match id {
+      0 => insts.iter().map(part2).sum(),
+      1 => insts.iter().map(part2).product(),
+      2 => insts.iter().map(part2).min().unwrap(),
+      3 => insts.iter().map(part2).max().unwrap(),
+      5 => if part2(&insts[0]) >  part2(&insts[1]) {1} else {0}
+      6 => if part2(&insts[0]) <  part2(&insts[1]) {1} else {0}
+      7 => if part2(&insts[0]) == part2(&insts[1]) {1} else {0}
+      _ => unreachable!()
+    }
+  }
+}
+
+aoc2021::main! {
+  let b = decode_hex(&INPUT);
+  let inst = parse_instruction(&b, 1);
+  let p1 = part1(&inst.1[0]);
+  let p2 = part2(&inst.1[0]);
+  (p1,p2)
+}
