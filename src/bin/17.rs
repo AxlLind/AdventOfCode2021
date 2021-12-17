86c66dca764740ed00d21354f6d43c2beb9c216a
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2021-12-17 08:24:41 +0100

    17: make less hardcoded

--- src/bin/17.rs
+++ src/bin/17.rs
@@ -1,8 +1,9 @@
-use std::ops::RangeInclusive;
 use itertools::Itertools;
 
-static TARGETX: RangeInclusive<i32> = 25..=67;
-static TARGETY: RangeInclusive<i32> = -260..=-200;
+const XMIN: i32 = 25;
+const XMAX: i32 = 67;
+const YMIN: i32 = -260;
+const YMAX: i32 = -200;
 
 fn try_vel(mut dx: i32, mut dy: i32) -> Option<i32> {
   let (mut x, mut y, mut maxy) = (0,0,0);
@@ -12,17 +13,17 @@ fn try_vel(mut dx: i32, mut dy: i32) -> Option<i32> {
     dx -= dx.signum();
     dy -= 1;
     if y > maxy { maxy = y; }
-    match (TARGETX.contains(&x), TARGETY.contains(&y)) {
+    match (XMIN <= x && x <= XMAX, YMIN <= y && y <= YMAX) {
       (true,true) => return Some(maxy),
       (false,_) if dx == 0 => return None,
-      (_,false) if dy < 0 && y < -260 => return None,
+      (_,false) if dy < 0 && y < YMIN => return None,
       _ => {}
     }
   }
 }
 
 aoc2021::main! {
-  let maxys = (0..=67).cartesian_product(-260..1000)
+  let maxys = (0..=XMAX).cartesian_product(YMIN..1000)
     .filter_map(|(x,y)| try_vel(x,y))
     .collect::<Vec<_>>();
   (*maxys.iter().max().unwrap(), maxys.len())
