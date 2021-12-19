b1a4b5af5c0e3e68a7bf5c71310eb28d9a1a65af
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2021-12-19 10:42:14 +0100

    19: better variable naming

--- src/bin/19.rs
+++ src/bin/19.rs
@@ -33,13 +33,13 @@ fn rotate([x,y,z]: [i32;3], rot: u8) -> [i32;3] {
   }
 }
 
-fn merge_scan(total_scan: &mut HashSet<[i32;3]>, b: &[[i32;3]], rot: u8) -> Option<[i32;3]> {
-  let b = b.iter().map(|&v| rotate(v, rot)).collect::<Vec<_>>();
+fn merge_scan(total_scan: &mut HashSet<[i32;3]>, scan: &[[i32;3]], rot: u8) -> Option<[i32;3]> {
+  let rotated_scan = scan.iter().map(|&v| rotate(v, rot)).collect::<Vec<_>>();
   let distances = total_scan.iter()
-    .cartesian_product(&b)
+    .cartesian_product(&rotated_scan)
     .map(|([x1,y1,z1], [x2,y2,z2])| [x1-x2, y1-y2, z1-z2]);
   for [dx,dy,dz] in distances {
-    let translated = b.iter().map(|[x3,y3,z3]| [x3+dx, y3+dy, z3+dz]);
+    let translated = rotated_scan.iter().map(|[x3,y3,z3]| [x3+dx, y3+dy, z3+dz]);
     if translated.clone().filter(|v| total_scan.contains(v)).count() >= 12 {
       total_scan.extend(translated);
       return Some([dx,dy,dz]);
@@ -50,8 +50,7 @@ fn merge_scan(total_scan: &mut HashSet<[i32;3]>, b: &[[i32;3]], rot: u8) -> Opti
 
 aoc2021::main! {
   let mut scans = INPUT.split("\n\n")
-    .map(|s| s.lines()
-      .skip(1)
+    .map(|s| s.lines().skip(1)
       .map(|l| {
         let (a,tmp) = l.split_once(',').unwrap();
         let (b,c) = tmp.split_once(',').unwrap();
@@ -60,13 +59,13 @@ aoc2021::main! {
       .collect::<Vec<_>>()
     )
     .collect::<Vec<_>>();
-  let mut total_scan = scans.swap_remove(0).into_iter().collect::<HashSet<_>>();
+  let mut total_scan = scans.remove(0).into_iter().collect::<HashSet<_>>();
   let mut dists = Vec::new();
   while !scans.is_empty() {
     for i in (0..scans.len()).rev() {
       if let Some(d) = (0..24).find_map(|rot| merge_scan(&mut total_scan, &scans[i], rot)) {
         dists.push(d);
-        scans.remove(i);
+        scans.swap_remove(i);
       }
     }
   }
