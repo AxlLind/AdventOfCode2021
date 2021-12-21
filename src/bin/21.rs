1dab63400d1650f0811ff031bcd0bbae70747331
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2021-12-21 07:10:01 +0100

    21: simplify part 2

--- src/bin/21.rs
+++ src/bin/21.rs
@@ -1,7 +1,7 @@
 use hashbrown::HashMap;
 use itertools::iproduct;
 
-type Cache = HashMap<(usize,usize,usize,usize,bool),(usize,usize)>;
+type Cache = HashMap<(usize,usize,usize,usize),(usize,usize)>;
 
 fn regular_game(mut pos1: usize, mut pos2: usize) -> usize {
   let (mut p1, mut p2, mut die, mut nrolls) = (0,0,1,0);
@@ -27,33 +27,27 @@ fn regular_game(mut pos1: usize, mut pos2: usize) -> usize {
   nrolls * if p1 >= 1000 {p2} else {p1}
 }
 
-fn quantum_game(cache: &mut Cache, p1: usize, p2: usize, pos1: usize, pos2: usize, turn1: bool) -> (usize,usize) {
+fn quantum_game(cache: &mut Cache, p1: usize, p2: usize, pos1: usize, pos2: usize) -> (usize,usize) {
   if p1 >= 21 { return (1,0); }
   if p2 >= 21 { return (0,1); }
-  if let Some(&score) = cache.get(&(p1,p2,pos1,pos2,turn1))  { return score; }
-  if let Some(&score) = cache.get(&(p2,p1,pos2,pos1,!turn1)) { return (score.1,score.0); }
+  if let Some(&score) = cache.get(&(p1,p2,pos1,pos2)) { return score; }
 
   let mut score = (0,0);
   for (d1,d2,d3) in iproduct!([1,2,3],[1,2,3],[1,2,3]) {
     let die = d1+d2+d3;
-    let (s1,s2) = if turn1 {
-      let pos1 = pos1 + die - if pos1+die > 10 {10} else {0};
-      quantum_game(cache,p1+pos1,p2,pos1,pos2,false)
-    } else {
-      let pos2 = pos2 + die - if pos2+die > 10 {10} else {0};
-      quantum_game(cache,p1,p2+pos2,pos1,pos2,true)
-    };
-    score.0 += s1;
-    score.1 += s2;
+    let pos1 = pos1 + die - if pos1+die > 10 {10} else {0};
+    let (s1,s2) = quantum_game(cache,p2,p1+pos1,pos2,pos1);
+    score.0 += s2;
+    score.1 += s1;
   }
 
-  cache.insert((p1,p2,pos1,pos2,turn1), score);
+  cache.insert((p1,p2,pos1,pos2), score);
   score
 }
 
 aoc2021::main! {
   let p1 = regular_game(9,6);
-  let (s1,s2) = quantum_game(&mut HashMap::new(),0,0,9,6,true);
+  let (s1,s2) = quantum_game(&mut HashMap::new(),0,0,9,6);
   let p2 = if s1 > s2 {s1} else {s2};
   (p1,p2)
 }
