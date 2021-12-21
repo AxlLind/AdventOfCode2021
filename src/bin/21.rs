0a1334ba7c5e838a01b5cd5d5383572145b29851
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2021-12-21 06:38:15 +0100

    Finish day 21

---
+++ src/bin/21.rs
@@ -0,0 +1,59 @@
+use hashbrown::HashMap;
+use itertools::Itertools;
+
+type Cache = HashMap<(usize,usize,usize,usize,bool),(usize,usize)>;
+
+fn part1(mut pos1: usize, mut pos2: usize) -> usize {
+  let (mut p1, mut p2, mut die, mut nrolls) = (0,0,1,0);
+  loop {
+    for _ in 0..3 {
+      if die > 100 { die = 1; }
+      pos1 += die;
+      die = (die + 1) % 100;
+    }
+    nrolls += 3;
+    while pos1 > 10 { pos1 -= 10 }
+    p1 += pos1;
+    if p1 >= 1000 { break }
+
+    for _ in 0..3 {
+      if die > 100 { die = 1; }
+      pos2 += die;
+      die = (die + 1) % 100;
+    }
+    nrolls += 3;
+    while pos2 > 10 { pos2 -= 10 }
+    p2 += pos2;
+    if p2 >= 1000 { break }
+  }
+  nrolls * if p1 >= 1000 {p2} else {p1}
+}
+
+fn play_game(cache: &mut Cache, p1: usize, p2: usize, pos1: usize, pos2: usize, turn1: bool) -> (usize,usize) {
+  if p1 >= 21 { return (1,0); }
+  if p2 >= 21 { return (0,1); }
+  if let Some(&score) = cache.get(&(p1,p2,pos1,pos2,turn1)) {
+    return score;
+  }
+  let mut score = (0,0);
+  for ((d1,d2),d3) in [1,2,3].iter().cartesian_product([1,2,3]).cartesian_product([1,2,3]) {
+    let die = d1+d2+d3;
+    let (s1,s2) = if turn1 {
+      let pos1 = pos1 + die - if pos1+die > 10 {10} else {0};
+      play_game(cache,p1+pos1,p2,pos1,pos2,false)
+    } else {
+      let pos2 = pos2 + die - if pos2+die > 10 {10} else {0};
+      play_game(cache,p1,p2+pos2,pos1,pos2,true)
+    };
+    score.0 += s1;
+    score.1 += s2;
+  }
+  cache.insert((p1,p2,pos1,pos2,turn1), score);
+  score
+}
+
+aoc2021::main! {
+  let p1 = part1(9,6);
+  let (p2,_) = play_game(&mut HashMap::new(),0,0,9,6,true);
+  (p1,p2)
+}
