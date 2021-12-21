b0e11cd5f3ef527a47a2c33910dcd75ae58d3031
Axel Lindeberg <axel.lindeberg@subset.se>
2021-12-21 08:11:39 +0100

    21: rename variables for clarity

--- src/bin/21.rs
+++ src/bin/21.rs
@@ -5,36 +5,36 @@ use itertools::iproduct;
 type Cache = HashMap<(usize,usize,usize,usize),(usize,usize)>;
 
 fn regular_game(mut pos1: usize, mut pos2: usize) -> usize {
-  let (mut p1, mut p2, mut die, mut nrolls) = (0,0,1,0);
+  let (mut s1, mut s2, mut die, mut nrolls) = (0,0,1,0);
   loop {
     for _ in 0..3 {
       pos1 += die;
       die = (die + 1) % 100;
     }
     while pos1 > 10 { pos1 -= 10 }
-    p1 += pos1;
+    s1 += pos1;
     nrolls += 3;
-    if p1 >= 1000 { break; }
+    if s1 >= 1000 { break; }
     swap(&mut pos1, &mut pos2);
-    swap(&mut p1, &mut p2);
+    swap(&mut s1, &mut s2);
   }
-  nrolls * p2
+  nrolls * s2
 }
 
-fn quantum_game(cache: &mut Cache, p1: usize, p2: usize, pos1: usize, pos2: usize) -> (usize,usize) {
-  if p2 >= 21 { return (0,1); }
-  if let Some(&score) = cache.get(&(p1,p2,pos1,pos2)) { return score; }
+fn quantum_game(cache: &mut Cache, s1: usize, s2: usize, pos1: usize, pos2: usize) -> (usize,usize) {
+  if s2 >= 21 { return (0,1); }
+  if let Some(&score) = cache.get(&(s1,s2,pos1,pos2)) { return score; }
 
   let mut score = (0,0);
   for (d1,d2,d3) in iproduct!([1,2,3],[1,2,3],[1,2,3]) {
     let die = d1+d2+d3;
     let pos1 = pos1 + die - if pos1+die > 10 {10} else {0};
-    let (s1,s2) = quantum_game(cache,p2,p1+pos1,pos2,pos1);
+    let (s1,s2) = quantum_game(cache,s2,s1+pos1,pos2,pos1);
     score.0 += s2;
     score.1 += s1;
   }
 
-  cache.insert((p1,p2,pos1,pos2), score);
+  cache.insert((s1,s2,pos1,pos2), score);
   score
 }
 
