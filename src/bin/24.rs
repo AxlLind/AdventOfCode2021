8c6a7a6f1c5f32de75a7e12811cc5c8a799328ca
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2021-12-25 00:39:38 +0100

    24: use visited set instead of hashmap

--- src/bin/24.rs
+++ src/bin/24.rs
@@ -1,8 +1,9 @@
-use hashbrown::HashMap;
+use hashbrown::HashSet;
 
 static INPUT: &str = "inp w\nmul x 0\nadd x z\nmod x 26\ndiv z 1\nadd x 10\neql x w\neql x 0\nmul y 0\nadd y 25\nmul y x\nadd y 1\nmul z y\nmul y 0\nadd y w\nadd y 2\nmul y x\nadd z y\ninp w\nmul x 0\nadd x z\nmod x 26\ndiv z 1\nadd x 15\neql x w\neql x 0\nmul y 0\nadd y 25\nmul y x\nadd y 1\nmul z y\nmul y 0\nadd y w\nadd y 16\nmul y x\nadd z y\ninp w\nmul x 0\nadd x z\nmod x 26\ndiv z 1\nadd x 14\neql x w\neql x 0\nmul y 0\nadd y 25\nmul y x\nadd y 1\nmul z y\nmul y 0\nadd y w\nadd y 9\nmul y x\nadd z y\ninp w\nmul x 0\nadd x z\nmod x 26\ndiv z 1\nadd x 15\neql x w\neql x 0\nmul y 0\nadd y 25\nmul y x\nadd y 1\nmul z y\nmul y 0\nadd y w\nadd y 0\nmul y x\nadd z y\ninp w\nmul x 0\nadd x z\nmod x 26\ndiv z 26\nadd x -8\neql x w\neql x 0\nmul y 0\nadd y 25\nmul y x\nadd y 1\nmul z y\nmul y 0\nadd y w\nadd y 1\nmul y x\nadd z y\ninp w\nmul x 0\nadd x z\nmod x 26\ndiv z 1\nadd x 10\neql x w\neql x 0\nmul y 0\nadd y 25\nmul y x\nadd y 1\nmul z y\nmul y 0\nadd y w\nadd y 12\nmul y x\nadd z y\ninp w\nmul x 0\nadd x z\nmod x 26\ndiv z 26\nadd x -16\neql x w\neql x 0\nmul y 0\nadd y 25\nmul y x\nadd y 1\nmul z y\nmul y 0\nadd y w\nadd y 6\nmul y x\nadd z y\ninp w\nmul x 0\nadd x z\nmod x 26\ndiv z 26\nadd x -4\neql x w\neql x 0\nmul y 0\nadd y 25\nmul y x\nadd y 1\nmul z y\nmul y 0\nadd y w\nadd y 6\nmul y x\nadd z y\ninp w\nmul x 0\nadd x z\nmod x 26\ndiv z 1\nadd x 11\neql x w\neql x 0\nmul y 0\nadd y 25\nmul y x\nadd y 1\nmul z y\nmul y 0\nadd y w\nadd y 3\nmul y x\nadd z y\ninp w\nmul x 0\nadd x z\nmod x 26\ndiv z 26\nadd x -3\neql x w\neql x 0\nmul y 0\nadd y 25\nmul y x\nadd y 1\nmul z y\nmul y 0\nadd y w\nadd y 5\nmul y x\nadd z y\ninp w\nmul x 0\nadd x z\nmod x 26\ndiv z 1\nadd x 12\neql x w\neql x 0\nmul y 0\nadd y 25\nmul y x\nadd y 1\nmul z y\nmul y 0\nadd y w\nadd y 9\nmul y x\nadd z y\ninp w\nmul x 0\nadd x z\nmod x 26\ndiv z 26\nadd x -7\neql x w\neql x 0\nmul y 0\nadd y 25\nmul y x\nadd y 1\nmul z y\nmul y 0\nadd y w\nadd y 3\nmul y x\nadd z y\ninp w\nmul x 0\nadd x z\nmod x 26\ndiv z 26\nadd x -15\neql x w\neql x 0\nmul y 0\nadd y 25\nmul y x\nadd y 1\nmul z y\nmul y 0\nadd y w\nadd y 2\nmul y x\nadd z y\ninp w\nmul x 0\nadd x z\nmod x 26\ndiv z 26\nadd x -7\neql x w\neql x 0\nmul y 0\nadd y 25\nmul y x\nadd y 1\nmul z y\nmul y 0\nadd y w\nadd y 3\nmul y x\nadd z y";
 
-type Cache = HashMap<(i64, usize), Option<i64>>;
+#[derive(Clone, Copy)]
+enum Op { Add, Mul, Div, Mod, Eql }
 
 #[derive(Clone, Copy)]
 enum Source { Reg(usize), Val(i64) }
@@ -26,68 +27,57 @@ impl Source {
   }
 }
 
-#[derive(Clone, Copy)]
-enum Instruction {
-  Inp(usize),
-  Add(usize, Source),
-  Mul(usize, Source),
-  Div(usize, Source),
-  Mod(usize, Source),
-  Eql(usize, Source),
-}
-
-fn find_modelnum(memo: &mut Cache, blocks: &[Vec<Instruction>], block: usize, z: i64, range: &[i64;9]) -> Option<i64> {
+fn find_modelnum(visited: &mut HashSet<(i64, usize)>, blocks: &[Vec<(Op,usize,Source)>], block: usize, z: i64, range: &[i64;9]) -> Option<i64> {
   if block == blocks.len() {
     return if z == 0 {Some(0)} else {None};
   }
-  if let Some(&answer) = memo.get(&(z,block)) { return answer; }
+  if visited.contains(&(z,block)) { return None; }
 
   for &digit in range {
     let mut regs = [digit,0,0,z];
     for &inst in &blocks[block] {
       match inst {
-        Instruction::Add(a,b) => regs[a] += b.val(&regs),
-        Instruction::Mul(a,b) => regs[a] *= b.val(&regs),
-        Instruction::Div(a,b) => regs[a] /= b.val(&regs),
-        Instruction::Mod(a,b) => regs[a] %= b.val(&regs),
-        Instruction::Eql(a,b) => regs[a] = (regs[a] == b.val(&regs)) as i64,
-        Instruction::Inp(_) => unreachable!()
+        (Op::Add, a,b) => regs[a] += b.val(&regs),
+        (Op::Mul, a,b) => regs[a] *= b.val(&regs),
+        (Op::Div, a,b) => regs[a] /= b.val(&regs),
+        (Op::Mod, a,b) => regs[a] %= b.val(&regs),
+        (Op::Eql, a,b) => regs[a] = (regs[a] == b.val(&regs)) as i64,
       }
     }
-    if let Some(best) = find_modelnum(memo, blocks, block+1, regs[3], range) {
-      memo.insert((regs[3],block), Some(best*10 + digit));
+    if let Some(best) = find_modelnum(visited, blocks, block+1, regs[3], range) {
       return Some(best*10 + digit)
     }
   }
-
-  memo.insert((z,block),None);
+  visited.insert((z,block));
   None
 }
 
-fn solve(blocks: &[Vec<Instruction>], biggest: bool) -> String {
+fn solve(blocks: &[Vec<(Op,usize,Source)>], biggest: bool) -> String {
   let range = if biggest {[9,8,7,6,5,4,3,2,1]} else {[1,2,3,4,5,6,7,8,9]};
-  let answer = find_modelnum(&mut Cache::new(), &blocks, 0, 0, &range).unwrap();
+  let answer = find_modelnum(&mut HashSet::new(), &blocks, 0, 0, &range).unwrap();
   answer.to_string().chars().rev().collect()
 }
 
 aoc2021::main! {
   let insts = INPUT.lines()
+    .filter(|l| !l.starts_with("inp"))
     .map(|l| {
+      let op =  match &l[..3] {
+        "add" => Op::Add,
+        "mul" => Op::Mul,
+        "div" => Op::Div,
+        "mod" => Op::Mod,
+        "eql" => Op::Eql,
+        _ => unreachable!()
+      };
       let src = match Source::from_str(&l[4..5]) {
         Source::Reg(src) => src,
         _ => unreachable!()
       };
-      match &l[..3] {
-        "inp" => Instruction::Inp(src),
-        "add" => Instruction::Add(src, Source::from_str(&l[6..])),
-        "mul" => Instruction::Mul(src, Source::from_str(&l[6..])),
-        "div" => Instruction::Div(src, Source::from_str(&l[6..])),
-        "mod" => Instruction::Mod(src, Source::from_str(&l[6..])),
-        "eql" => Instruction::Eql(src, Source::from_str(&l[6..])),
-        _ => unreachable!()
-      }
+      let dest = Source::from_str(&l[6..]);
+      (op,src,dest)
     })
     .collect::<Vec<_>>();
-  let blocks = insts.chunks(18).map(|c| c.iter().skip(1).copied().collect()).collect::<Vec<_>>();
+  let blocks = insts.chunks(17).map(|c| c.iter().copied().collect()).collect::<Vec<_>>();
   (solve(&blocks, true), solve(&blocks, false))
 }
