bf1bf5c5fad683e3ce91abdf38e2c3f55013c371
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2021-12-25 14:58:10 +0100

    23: 60% speed up by reducing state size

--- src/bin/23.rs
+++ src/bin/23.rs
@@ -5,7 +5,7 @@ use itertools::Itertools;
 static INPUT: &str = "#############\n#...........#\n###C#A#B#D###\n  #D#C#A#B#  \n  #########  ";
 
 fn right_configuration(maze: &Vec<Vec<u8>>) -> bool {
-  maze[2..(maze.len()-1)].iter().all(|l| itertools::equal(l[3..10].iter().copied(), "A#B#C#D".bytes()))
+  maze.iter().skip(1).all(|l| itertools::equal(l[3..10].iter().copied(), "A#B#C#D".bytes()))
 }
 
 fn make_move(maze: &Vec<Vec<u8>>, x0: usize, y0: usize, x1: usize, y1: usize) -> Vec<Vec<u8>> {
@@ -16,11 +16,11 @@ fn make_move(maze: &Vec<Vec<u8>>, x0: usize, y0: usize, x1: usize, y1: usize) ->
 }
 
 fn moves(maze: &Vec<Vec<u8>>) -> Vec<(usize,Vec<Vec<u8>>)> {
-  let room_len = maze.len() - 2;
+  let room_len = maze.len() - 1;
   let mut moves = Vec::new();
-  for y in 0..maze[1].len() {
+  for y in 0..maze[0].len() {
     // check moving into a room
-    let (room,exp) = match maze[1][y] {
+    let (room,exp) = match maze[0][y] {
       b'A' => (3,1),
       b'B' => (5,10),
       b'C' => (7,100),
@@ -28,17 +28,17 @@ fn moves(maze: &Vec<Vec<u8>>) -> Vec<(usize,Vec<Vec<u8>>)> {
       _ => continue,
     };
     let (r0,r1) = if y > room {(room,y)} else {(y+1,room+1)};
-    if (r0..r1).any(|i| maze[1][i] != b'.') {
+    if (r0..r1).any(|i| maze[0][i] != b'.') {
       continue;
     }
-    let i = match (2..=room_len).take_while(|&i| maze[i][room] == b'.').last() {
+    let i = match (1..=room_len).take_while(|&i| maze[i][room] == b'.').last() {
       Some(i) => i,
       _ => continue
     };
-    if i != room_len && maze[i+1][room] != maze[1][y] { continue; }
-    moves.push(((r1-r0 + i-1) * exp, make_move(maze, 1, y, i, room)));
+    if i != room_len && maze[i+1][room] != maze[0][y] { continue; }
+    moves.push(((r1-r0 + i) * exp, make_move(maze, 0, y, i, room)));
   }
-  for (x,y) in (2..=room_len).cartesian_product([3,5,7,9]) {
+  for (x,y) in (1..=room_len).cartesian_product([3,5,7,9]) {
     // check moving out of a room
     let exp = match maze[x][y] {
       b'A' => 1,
@@ -47,18 +47,18 @@ fn moves(maze: &Vec<Vec<u8>>) -> Vec<(usize,Vec<Vec<u8>>)> {
       b'D' => 1000,
       _ => continue,
     };
-    if (2..x).any(|i| maze[i][y] != b'.') || (x+1..=room_len).any(|i| maze[i][y] == b'.') {
+    if (1..x).any(|i| maze[i][y] != b'.') || (x+1..=room_len).any(|i| maze[i][y] == b'.') {
       continue;
     }
     for i in y..maze[0].len() { // move left
-      if maze[1][i] != b'.' { break; }
+      if maze[0][i] != b'.' { break; }
       if ![1,2,4,6,8,10,11].contains(&i) { continue; }
-      moves.push(((x-1 + i-y) * exp, make_move(maze, x, y, 1, i)));
+      moves.push(((x + i-y) * exp, make_move(maze, x, y, 0, i)));
     }
     for i in (1..=y).rev() { // move right
-      if maze[1][i] != b'.' { break; }
+      if maze[0][i] != b'.' { break; }
       if ![1,2,4,6,8,10,11].contains(&i) { continue; }
-      moves.push(((x-1 + y-i) * exp,make_move(maze, x, y, 1, i)));
+      moves.push(((x + y-i) * exp,make_move(maze, x, y, 0, i)));
     }
   }
   moves
@@ -86,10 +86,10 @@ fn shortest_path(maze: &Vec<Vec<u8>>) -> i64 {
 }
 
 aoc2021::main! {
-  let mut map = INPUT.lines().map(|l| l.bytes().collect()).collect();
+  let mut map = INPUT.lines().skip(1).take(3).map(|l| l.bytes().collect()).collect();
   let p1 = shortest_path(&map);
-  map.insert(4, "  #D#C#B#A#  ".bytes().collect());
-  map.insert(4, "  #D#B#A#C#  ".bytes().collect());
+  map.push("  #D#C#B#A#  ".bytes().collect());
+  map.push("  #D#B#A#C#  ".bytes().collect());
   let p2 = shortest_path(&map);
   (p1,p2)
 }
