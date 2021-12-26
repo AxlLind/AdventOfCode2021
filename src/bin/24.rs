6aa35ac228ef9388c119b431a0bb414679708bfb
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2021-12-26 09:53:53 +0100

    24: simplify main

--- src/bin/24.rs
+++ src/bin/24.rs
@@ -24,29 +24,28 @@ static INPUT: &str = "inp w\nmul x 0\nadd x z\nmod x 26\ndiv z 1\nadd x 10\neql 
 // => X = (Z%26 + B) != I
 //    Z = (Z/A) * (25 * X + 1) + (I + C) * X
 
-fn find_modelnum(visited: &mut HashSet<(i64, usize)>, blocks: &[(i64,i64,i64)], block: usize, z: i64, range: &[i64;9]) -> Option<i64> {
+fn find_modelnum(cache: &mut HashSet<(i64, usize)>, blocks: &[(i64,i64,i64)], block: usize, z: i64, range: &[i64]) -> Option<i64> {
   if block == blocks.len() {
     return if z == 0 {Some(0)} else {None};
   }
-  if visited.contains(&(z,block)) {
+  if cache.contains(&(z,block)) {
     return None;
   }
   let (a,b,c) = blocks[block];
   for &i in range {
     let x = (z % 26 + b != i) as i64;
     let z = (z/a) * (25*x + 1) + (i + c) * x;
-    if let Some(n) = find_modelnum(visited, blocks, block+1, z, range) {
+    if let Some(n) = find_modelnum(cache, blocks, block+1, z, range) {
       return Some(n*10 + i)
     }
   }
-  visited.insert((z,block));
+  cache.insert((z,block));
   None
 }
 
-fn solve(blocks: &[(i64,i64,i64)], biggest: bool) -> String {
-  let range = if biggest {[9,8,7,6,5,4,3,2,1]} else {[1,2,3,4,5,6,7,8,9]};
-  let answer = find_modelnum(&mut HashSet::new(), &blocks, 0, 0, &range).unwrap();
-  answer.to_string().chars().rev().collect()
+fn solve(blocks: &[(i64,i64,i64)], range: [i64;9]) -> String {
+  let ans = find_modelnum(&mut HashSet::new(), &blocks, 0, 0, &range).unwrap();
+  ans.to_string().chars().rev().collect()
 }
 
 aoc2021::main! {
@@ -59,5 +58,7 @@ aoc2021::main! {
       (a,b,c)
     })
     .collect::<Vec<_>>();
-  (solve(&blocks, true), solve(&blocks, false))
+  let p1 = solve(&blocks, [9,8,7,6,5,4,3,2,1]);
+  let p2 = solve(&blocks, [1,2,3,4,5,6,7,8,9]);
+  (p1,p2)
 }
