750c458906b24f6600d5559b4f7e3aeee106ad9e
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2022-01-01 13:18:12 +0100

    Finish day 14

---
+++ src/14.py
@@ -0,0 +1,19 @@
+from aoc import main
+
+INPUT = 640441
+
+def solve():
+  res = [int(c) for c in str(INPUT)]
+  e1,e2,recipes = 0,1,[3,7]
+  while recipes[-len(res):] != res:
+    x = recipes[e1] + recipes[e2]
+    if x > 9:
+      recipes.append(1)
+      if recipes[-len(res):] == res:
+        break
+    recipes.append(x % 10)
+    e1 = (e1 + recipes[e1] + 1) % len(recipes)
+    e2 = (e2 + recipes[e2] + 1) % len(recipes)
+  return ''.join(str(i) for i in recipes[INPUT:INPUT+10]),len(recipes) - len(res)
+
+main(solve)
