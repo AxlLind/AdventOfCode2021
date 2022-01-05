c2dd16dd2259b81c3cdf04243ed13f6ffcd99843
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2022-01-05 15:50:16 +0100

    01: return instead of break

--- src/01.py
+++ src/01.py
@@ -3,14 +3,12 @@ from itertools import cycle
 
 @aoc.main('01')
 def main(indata: str) -> tuple[int,int]:
-  nums = [int(line) for line in indata.split('\n')]
-  seen, s = set(), 0
+  nums, seen, s = [int(l) for l in indata.split('\n')], set(), 0
   for x in cycle(nums):
     s += x
     if s in seen:
-      break
+      return sum(nums), s
     seen.add(s)
-  return sum(nums), s
 
 if __name__ == "__main__":
   main()
