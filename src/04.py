2f6b109c643c8522ce3b49e0465c552c7419b5bb
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2022-01-05 16:18:01 +0100

    04: clean up

--- src/04.py
+++ src/04.py
@@ -9,21 +9,18 @@ def main(indata: str) -> list[int]:
     xs = re.findall("\[1518-(\d+)-(\d+) (\d+):(\d+)\] (.+)", l)[0]
     month, day, hour, minute = [int(i) for i in xs[:4]]
     actions.append([month, day, hour, minute, xs[4].split(' ')[1]])
-  actions = sorted(actions)
-
-  slept = defaultdict[str, Counter[int]](Counter)
-  asleep, guard = 0, ""
-  for _, _, _, minute, action in actions:
-    if action == 'up':
-      slept[guard].update(range(asleep, minute))
-    elif action == 'asleep':
-      asleep = minute
-    else:
-      guard = action[1:]
-
+  asleep, guard, slept = 0, -1, defaultdict[int, Counter[int]](Counter)
+  for _, _, _, minute, action in sorted(actions):
+    match action:
+      case 'up':
+        slept[guard].update(range(asleep, minute))
+      case 'asleep':
+        asleep = minute
+      case _:
+        guard = int(action[1:])
   g1 = max(slept, key=lambda g: sum(slept[g].values()))
   g2 = max(slept, key=lambda g: slept[g].most_common(1)[0][1])
-  return [int(g) * slept[g].most_common(1)[0][0] for g in [g1,g2]]
+  return [g * slept[g].most_common(1)[0][0] for g in [g1,g2]]
 
 if __name__ == "__main__":
   main()
