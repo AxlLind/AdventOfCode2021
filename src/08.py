5fceec25d87b2a2e0c54cbca71086dd4e7d0591c
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2022-01-05 16:30:34 +0100

    08: better function names

--- src/08.py
+++ src/08.py
@@ -3,27 +3,27 @@ from typing import Any
 
 Tree = tuple[list[Any],list[int]] # mypy cannot handle recursive types, use 'Any' for now
 
-def parse(xs: list[int], i: int) -> tuple[int,Tree]:
+def parse_tree(xs: list[int], i: int) -> tuple[int,Tree]:
   children, ndata = [], xs[i+1]
   i += 2
   for _ in range(xs[i-2]):
-    i, child = parse(xs,i)
+    i, child = parse_tree(xs,i)
     children.append(child)
   return i+ndata, (children, xs[i:i+ndata])
 
-def part1(e: Tree) -> int:
-  return sum(e[1]) + sum(part1(c) for c in e[0])
+def metadata_sum(e: Tree) -> int:
+  return sum(e[1]) + sum(metadata_sum(c) for c in e[0])
 
-def part2(e: Tree) -> int:
+def node_value(e: Tree) -> int:
   cs, data = e
   if len(cs) == 0:
     return sum(data)
-  return sum(part2(cs[c-1]) for c in data if 0 < c <= len(cs))
+  return sum(node_value(cs[c-1]) for c in data if 0 < c <= len(cs))
 
 @aoc.main('08')
 def main(indata: str) -> tuple[int,int]:
-  _,e = parse([int(i) for i in indata.split(' ')],0)
-  return part1(e), part2(e)
+  _,e = parse_tree([int(i) for i in indata.split(' ')],0)
+  return metadata_sum(e), node_value(e)
 
 if __name__ == "__main__":
   main()
