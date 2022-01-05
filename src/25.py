3c614789bebf2cec22e5825498cb896184c429bc
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2022-01-05 10:57:10 +0100

    25: better naming

--- src/25.py
+++ src/25.py
@@ -4,21 +4,21 @@ import re
 def dist(s1: list[int], s2: list[int]) -> int:
   return sum(abs(a-b) for a,b in zip(s1,s2))
 
-def remove_component(stars: list[list[int]], s: list[int]):
+def pop_constellation(stars: list[list[int]], s: list[int]):
   if s not in stars:
     return
   stars.remove(s)
   for s2 in [s2 for s2 in stars if dist(s,s2) < 4]:
-    remove_component(stars, s2)
+    pop_constellation(stars, s2)
 
 @aoc.main('25')
 def main(indata: str) -> tuple[int,str]:
   stars = [[int(i) for i in re.findall("-?\d+", l)] for l in indata.split('\n')]
-  components = 0
+  constellations = 0
   while stars:
-    remove_component(stars, stars[0])
-    components += 1
-  return components, '🎄'
+    pop_constellation(stars, stars[0])
+    constellations += 1
+  return constellations, '🎄'
 
 if __name__ == '__main__':
   main()
