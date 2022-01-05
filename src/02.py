467adc042e930833bde17c8da15ef95e2eb0b1c7
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2022-01-05 15:54:10 +0100

    02: clearer part 1

--- src/02.py
+++ src/02.py
@@ -3,11 +3,13 @@ from collections import Counter
 from itertools import combinations
 
 def part1(ids: list[str]) -> int:
-  c = Counter(x for l in ids for x in set(Counter(l).values()))
+  c = Counter()
+  for l in ids:
+    c.update(set(Counter(l).values()))
   return c[2]*c[3]
 
 def part2(ids: list[str]) -> str:
-  for a,b in combinations(ids,2):
+  for a,b in combinations(ids,r=2):
     ans = [c1 for c1,c2 in zip(a,b) if c1 == c2]
     if len(ans)+1 == len(a):
       return ''.join(ans)
