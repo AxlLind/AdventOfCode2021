216aa2549e3b077c01977fb2a4ba4673d13548d1
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2022-01-05 16:12:17 +0100

    03: remove unnecessary whitespace

--- src/03.py
+++ src/03.py
@@ -5,18 +5,14 @@ import re
 
 @aoc.main('03')
 def main(indata: str) -> tuple[int,int]:
-  squares = [[int(i) for i in re.findall("\d+", l)] for l in indata.split('\n')]
-
+  squares = [[int(i) for i in re.findall("\d+",l)] for l in indata.split('\n')]
   points = defaultdict[tuple[int,int],int](int)
   for _,x,y,h,w in squares:
     for p in product(range(x,x+h), range(y,y+w)):
       points[p] += 1
-
   for p2,x,y,h,w in squares:
     if all(points[p] == 1 for p in product(range(x,x+h), range(y,y+w))):
-      break
-
-  return sum(c > 1 for c in points.values()), p2
+      return sum(c > 1 for c in points.values()), p2
 
 if __name__ == "__main__":
   main()
