f99c8b012bbb17cb83f360b2e1d081e7cd3fec5d
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2022-01-21 08:26:32 +0100

    Remove automatic fetching of inputs
    This was fun to implement but overall adds a lot of complexity
    and dependencies that are slow to compile.
    
    Will just use the fetch.sh script to fetch inputs instead.

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,6 @@
-use std::{fs, env, path::Path};
 use proc_macro::TokenStream;
 use quote::quote;
 use syn::{parse_macro_input, AttributeArgs, ItemFn, NestedMeta, Lit, Ident};
-use reqwest::blocking::Client;
-
-fn ensure_inputfile(day: &str) {
-  let path = format!("./inputs/{}.in", day);
-  if Path::new(&path).is_file() {
-    return;
-  }
-  let input = Client::new()
-    .get(format!("https://adventofcode.com/2021/day/{}/input", day.trim_start_matches('0')))
-    .header("cookie", format!("session={}", env::var("AOC_SESSION").expect("AOC_SESSION not set")))
-    .send()
-    .expect("http request failed")
-    .text()
-    .expect("malformed input response");
-  fs::write(path,input).expect("writing file failed");
-}
 
 #[proc_macro_attribute]
 pub fn main(args: TokenStream, input: TokenStream) -> TokenStream {
@@ -26,18 +9,16 @@ pub fn main(args: TokenStream, input: TokenStream) -> TokenStream {
     _ => panic!("Expected one string argument")
   };
 
-  let mut aocsolver = parse_macro_input!(input as ItemFn);
-  aocsolver.sig.ident = Ident::new("aocsolver", aocsolver.sig.ident.span());
-
-  ensure_inputfile(&day);
+  let mut aoc_solution = parse_macro_input!(input as ItemFn);
+  aoc_solution.sig.ident = Ident::new("aoc_solution", aoc_solution.sig.ident.span());
 
   let input_path = format!("../../inputs/{}.in", day);
   let tokens = quote! {
     fn main() {
-      #aocsolver
+      #aoc_solution
       let input = include_str!(#input_path);
       let now = ::std::time::Instant::now();
-      let (p1,p2) = aocsolver(input.trim_end());
+      let (p1,p2) = aoc_solution(input.trim_end());
       let time = now.elapsed().as_millis();
       println!("Part one: {}", p1);
       println!("Part two: {}", p2);
