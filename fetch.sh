b918f801d7af7f3690a4bbd41281069b60d82aa8
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2022-01-21 08:37:54 +0100

    Clean up fetch.sh

--- fetch.sh
+++ fetch.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
+set -euo pipefail
 
-# Fetches the input, prints to stdout and copies to clipboard.
-# This gives both a quick overview of what the input looks like
-# and makes it available on ctrl+v for use in the challenge.
-
-if [ -z "$1" ]; then
+if [[ $# != 1 ]]; then
   echo "Please provide a day number."
   echo "usage: $0 DAY"
   exit 1
 fi
 
-if [ -z "$AOC_SESSION" ]; then
-  echo "No session token."
+if [[ ! "$1" =~ ^(0[1-9]|1[0-9]|2[0-5])$ ]]; then
+  echo "Argument '$1' is not a valid day."
   exit 1
 fi
 
-URL="https://adventofcode.com/2021/day/$1/input"
-curl $URL --cookie "session=$AOC_SESSION" -s | tee >(pbcopy)
+if [[ -z "${AOC_SESSION-""}" ]]; then
+  echo "No session token set in \$AOC_SESSION."
+  exit 1
+fi
+
+URL="https://adventofcode.com/2021/day/$(("$1" + 0))/input"
+curl -f "$URL" --cookie "session=$AOC_SESSION" -s | tee "./inputs/$1.in"
