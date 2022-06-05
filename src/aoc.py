36a44dd7b0d46cccc50914efdb277beb43bcab9c
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2022-06-05 10:50:15 +0200

    Update aoc.py

--- src/aoc.py
+++ src/aoc.py
@@ -3,14 +3,14 @@ from typing import Any, Callable, Iterable
 from pathlib import Path
 
 INPUT_DIR = (Path(__file__) / '..' / '..' / 'inputs').resolve()
+AocSolution = Callable[[str], Iterable[Any]]
 
-def main(day: str) -> Callable[[Callable[[str], Iterable[Any]]],Callable[[],None]]:
-  def decorator(solver: Callable[[str], Iterable[Any]]) -> Callable[[],None]:
-    def timer():
-      with open(INPUT_DIR / f'{day}.in', 'r') as f:
-        indata = f.read().strip('\n')
+def main(day: str) -> Callable[[AocSolution], Callable[[],None]]:
+  def decorator(solver: AocSolution) -> Callable[[],None]:
+    def timer() -> None:
+      indata = (INPUT_DIR / f'{day}.in').read_text()
       start = time.time()
-      p1, p2 = solver(indata)
+      p1, p2 = solver(indata.rstrip('\n'))
       elapsed = int((time.time() - start) * 1000)
       print(f"Part 1: {p1}")
       print(f"Part 2: {p2}")
