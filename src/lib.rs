570783308fd91371eb14ae640b70f212e65ac6b7
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2022-10-17 09:22:19 +0200

    Fix incorrect panic message in proc macro

--- src/lib.rs
+++ src/lib.rs
@@ -4,21 +4,20 @@ use syn::{parse_macro_input, AttributeArgs, ItemFn, NestedMeta, Lit, Ident};
 
 #[proc_macro_attribute]
 pub fn main(args: TokenStream, input: TokenStream) -> TokenStream {
-  let day = match &parse_macro_input!(args as AttributeArgs)[..] {
-    [NestedMeta::Lit(Lit::Int(day))] => day.token().to_string(),
-    _ => panic!("Expected one string argument")
+  let input_path = match &parse_macro_input!(args as AttributeArgs)[..] {
+    [NestedMeta::Lit(Lit::Int(day))] => format!("../../inputs/{}.in", day.token().to_string()),
+    _ => panic!("Expected one integer argument"),
   };
 
   let mut aoc_solution = parse_macro_input!(input as ItemFn);
   aoc_solution.sig.ident = Ident::new("aoc_solution", aoc_solution.sig.ident.span());
 
-  let input_path = format!("../../inputs/{}.in", day);
   let tokens = quote! {
     const INPUT: &str = include_str!(#input_path);
     #aoc_solution
     fn main() {
       let now = ::std::time::Instant::now();
-      let (p1,p2) = aoc_solution(INPUT.trim_end());
+      let (p1, p2) = aoc_solution(INPUT.trim_end());
       let time = now.elapsed().as_millis();
       println!("Part one: {}", p1);
       println!("Part two: {}", p2);
