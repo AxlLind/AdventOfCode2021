7b359a15b2b858d358d3377215fa0fb1401c8620
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2022-12-04 08:54:15 +0100

    main: simplify program control flow

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,31 @@
-use std::{fs, process::Command, path::Path};
+use std::{fs, process::Command, error::Error};
 use itertools::Itertools;
 
-static BIN_DIR: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/src/bin");
+static BIN_DIR: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/src/bin/");
 
-fn extract_microseconds(output: &str) -> usize {
+fn extract_microseconds(output: &str) -> Result<usize, Box<dyn Error>> {
   let out = output.lines().last().unwrap();
-  if out.ends_with("ms") {
-    out["Time: ".len()..out.len()-2].parse::<usize>().unwrap() * 1000
+  let time = if out.ends_with("ms") {
+    out["Time: ".len()..out.len()-2].parse::<usize>()? * 1000
   } else {
-    out["Time: ".len()..out.len()-3].parse::<usize>().unwrap()
-  }
+    out["Time: ".len()..out.len()-3].parse::<usize>()?
+  };
+  Ok(time)
 }
 
-fn main() {
-  let total_time = fs::read_dir(&Path::new(BIN_DIR))
-    .unwrap()
-    .filter_map(|p| Some(p.ok()?.path().file_stem()?.to_str()?.to_string()))
+fn main() -> Result<(), Box<dyn Error>> {
+  let days = fs::read_dir(BIN_DIR)?
+    .filter_map(Result::ok)
+    .filter_map(|p| Some(p.path().file_stem()?.to_str()?.to_string()))
     .sorted()
-    .map(|day| {
-      let cmd = Command::new("cargo")
-        .args(&["run", "--release", "--bin", &day])
-        .output()
-        .unwrap();
-      let output = String::from_utf8(cmd.stdout).unwrap();
-      println!("Day {}:\n{}", day, output);
-      extract_microseconds(&output)
-    })
-    .sum::<usize>();
+    .collect::<Vec<_>>();
+  let mut total_time = 0;
+  for day in &days {
+    let cmd = Command::new("cargo").args(&["run", "--release", "--bin", day]).output()?;
+    let output = String::from_utf8(cmd.stdout)?;
+    println!("Day {}:\n{}", day, output);
+    total_time += extract_microseconds(&output)?;
+  }
   println!("Total time: {}ms", total_time / 1000);
+  Ok(())
 }
