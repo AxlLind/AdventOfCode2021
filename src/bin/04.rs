764fc200c86c0c52a91d5193e68267e80011536f
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2022-12-04 06:16:45 +0100

    Finish day 4

---
+++ src/bin/04.rs
@@ -0,0 +1,18 @@
+use std::cmp::{min, max};
+
+#[aoc::main(04)]
+fn main(input: &str) -> (usize, usize) {
+  let pairs = input.lines().filter_map(|l| {
+    let (x,y) = l.split_once(',')?;
+    let (a,b) = x.split_once('-')?;
+    let (c,d) = y.split_once('-')?;
+    Some((a.parse().ok()?, b.parse().ok()?, c.parse().ok()?, d.parse().ok()?))
+  }).collect::<Vec<(usize,_,_,_)>>();
+  let p1 = pairs.iter()
+    .filter(|(x1,y1,x2,y2)| (x1 <= x2 && y1 >= y2) || (x2 <= x1 && y2 >= y1))
+    .count();
+  let p2 = pairs.iter()
+    .filter(|(x1,y1,x2,y2)| max(x1,x2) <= min(y1,y2))
+    .count();
+  (p1, p2)
+}
