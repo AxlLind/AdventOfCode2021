bc0ebff15955e9facfe7a7831776e88022768e22
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2022-12-08 06:48:43 +0100

    08: simplify main

--- src/bin/08.rs
+++ src/bin/08.rs
@@ -1,11 +1,11 @@
+use std::cmp::max;
 use itertools::Itertools;
 
 fn examine_tree(grid: &Vec<Vec<u8>>, r: usize, c: usize) -> (bool, usize) {
   let tree = grid[r][c];
   let (mut invisible, mut score) = (true, 1);
   for (dr, dc) in [(-1,0), (0,-1), (1,0), (0,1)] {
-    let (mut r, mut c, mut i) = (r,c,0);
-    let mut visible = true;
+    let (mut r, mut c, mut i, mut visible) = (r, c, 0, true);
     while let Some(&next) = grid.get((r as isize + dr) as usize).and_then(|x| x.get((c as isize + dc) as usize)) {
       i += 1;
       if tree <= next {
@@ -28,12 +28,8 @@ fn main(input: &str) -> (usize, usize) {
   let grid = input.lines()
     .map(|l| l.as_bytes().iter().map(|b| b - b'0').collect::<Vec<_>>())
     .collect::<Vec<_>>();
-  let mut p1 = 0;
-  let mut p2 = 0;
-  for (r, c) in (0..grid.len()).cartesian_product(0..grid[0].len()) {
+  (0..grid.len()).cartesian_product(0..grid[0].len()).fold((0, 0), |(p1, p2), (r, c)| {
     let (visible, score) = examine_tree(&grid, r, c);
-    if visible { p1 += 1 }
-    if score > p2 { p2 = score }
-  }
-  (p1, p2)
+    (p1 + visible as usize, max(p2, score))
+  })
 }
