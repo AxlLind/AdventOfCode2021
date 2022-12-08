b58c4bae00e0e488e1e6d14e32a6d3d9c8d5c78f
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2022-12-08 06:38:36 +0100

    Finish day 8

---
+++ src/bin/08.rs
@@ -0,0 +1,39 @@
+use itertools::Itertools;
+
+fn examine_tree(grid: &Vec<Vec<u8>>, r: usize, c: usize) -> (bool, usize) {
+  let tree = grid[r][c];
+  let (mut invisible, mut score) = (true, 1);
+  for (dr, dc) in [(-1,0), (0,-1), (1,0), (0,1)] {
+    let (mut r, mut c, mut i) = (r,c,0);
+    let mut visible = true;
+    while let Some(&next) = grid.get((r as isize + dr) as usize).and_then(|x| x.get((c as isize + dc) as usize)) {
+      i += 1;
+      if tree <= next {
+        visible = false;
+        break;
+      }
+      r = (r as isize + dr) as usize;
+      c = (c as isize + dc) as usize;
+    }
+    if visible {
+      invisible = false;
+    }
+    score *= i;
+  }
+  (!invisible, score)
+}
+
+#[aoc::main(08)]
+fn main(input: &str) -> (usize, usize) {
+  let grid = input.lines()
+    .map(|l| l.as_bytes().iter().map(|b| b - b'0').collect::<Vec<_>>())
+    .collect::<Vec<_>>();
+  let mut p1 = 0;
+  let mut p2 = 0;
+  for (r, c) in (0..grid.len()).cartesian_product(0..grid[0].len()) {
+    let (visible, score) = examine_tree(&grid, r, c);
+    if visible { p1 += 1 }
+    if score > p2 { p2 = score }
+  }
+  (p1, p2)
+}
