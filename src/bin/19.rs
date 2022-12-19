f8e5cf4049ac32fac572836fe2338ce02658dee1
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2022-12-19 10:00:41 +0100

    19: use u32 instead of i32

--- src/bin/19.rs
+++ src/bin/19.rs
@@ -1,19 +1,18 @@
 use std::collections::VecDeque;
-
+use itertools::Itertools;
 use hashbrown::HashSet;
 use rayon::prelude::*;
-use itertools::Itertools;
 
-#[derive(Default, Hash, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Debug)]
+#[derive(Default, Clone, Copy, Hash, PartialEq, Eq)]
 struct State {
-  ore: i32,
-  ore_robots: i32,
-  clay: i32,
-  clay_robots: i32,
-  obsidian: i32,
-  obsidian_robots: i32,
-  geode: i32,
-  geode_robots: i32,
+  ore: u32,
+  ore_robots: u32,
+  clay: u32,
+  clay_robots: u32,
+  obsidian: u32,
+  obsidian_robots: u32,
+  geode: u32,
+  geode_robots: u32,
 }
 
 fn earn(mut state: State) -> State {
@@ -24,10 +23,9 @@ fn earn(mut state: State) -> State {
   state
 }
 
-fn max_geodes((ore_ore_cost, clay_ore_cost, obs_ore_cost, obs_clay_cost, geo_ore_cost, geo_obs_cost): (i32,i32,i32,i32,i32,i32), time: i32) -> i32 {
+fn max_geodes((ore_ore_cost, clay_ore_cost, obs_ore_cost, obs_clay_cost, geo_ore_cost, geo_obs_cost): (u32,u32,u32,u32,u32,u32), time: u32) -> u32 {
   let max_ore_cost = *[ore_ore_cost, clay_ore_cost, obs_ore_cost, geo_ore_cost].iter().max().unwrap();
-  let mut seen = HashSet::new();
-  let mut q = VecDeque::new();
+  let (mut seen, mut q) = (HashSet::new(), VecDeque::new());
   q.push_back((0, {
     let mut start = State::default();
     start.ore_robots = 1;
@@ -44,30 +42,30 @@ fn max_geodes((ore_ore_cost, clay_ore_cost, obs_ore_cost, obs_clay_cost, geo_ore
     }
     seen.insert(state);
     if state.ore_robots < max_ore_cost && state.ore >= ore_ore_cost {
-      let mut new_state = earn(state);
-      new_state.ore -= ore_ore_cost;
-      new_state.ore_robots += 1;
-      q.push_back((len+1, new_state));
+      let mut state = earn(state);
+      state.ore -= ore_ore_cost;
+      state.ore_robots += 1;
+      q.push_back((len+1, state));
     }
     if state.clay_robots < obs_clay_cost && state.ore >= clay_ore_cost {
-      let mut new_state = earn(state);
-      new_state.ore -= clay_ore_cost;
-      new_state.clay_robots += 1;
-      q.push_back((len+1, new_state));
+      let mut state = earn(state);
+      state.ore -= clay_ore_cost;
+      state.clay_robots += 1;
+      q.push_back((len+1, state));
     }
     if state.obsidian_robots < geo_obs_cost && state.ore >= obs_ore_cost && state.clay >= obs_clay_cost {
-      let mut new_state = earn(state);
-      new_state.ore -= obs_ore_cost;
-      new_state.clay -= obs_clay_cost;
-      new_state.obsidian_robots += 1;
-      q.push_back((len+1, new_state));
+      let mut state = earn(state);
+      state.ore -= obs_ore_cost;
+      state.clay -= obs_clay_cost;
+      state.obsidian_robots += 1;
+      q.push_back((len+1, state));
     }
     if state.ore >= geo_ore_cost && state.obsidian >= geo_obs_cost {
-      let mut new_state = earn(state);
-      new_state.ore -= geo_ore_cost;
-      new_state.obsidian -= geo_obs_cost;
-      new_state.geode_robots += 1;
-      q.push_back((len+1, new_state));
+      let mut state = earn(state);
+      state.ore -= geo_ore_cost;
+      state.obsidian -= geo_obs_cost;
+      state.geode_robots += 1;
+      q.push_back((len+1, state));
     } else {
       q.push_back((len+1, earn(state)));
     }
@@ -76,11 +74,15 @@ fn max_geodes((ore_ore_cost, clay_ore_cost, obs_ore_cost, obs_clay_cost, geo_ore
 }
 
 #[aoc::main(19)]
-fn main(input: &str) -> (i32, i32) {
-  let blueprints = input.lines().map(|l|
-    l.split(|c: char| !c.is_ascii_digit()).filter(|w| !w.is_empty()).skip(1).map(|w| w.parse::<i32>().unwrap()).collect_tuple().unwrap()
+fn main(input: &str) -> (u32, u32) {
+  let blueprints = input.lines().filter_map(|l|
+    l.split(|c: char| !c.is_ascii_digit())
+      .filter(|w| !w.is_empty())
+      .skip(1)
+      .map(|w| w.parse().unwrap())
+      .collect_tuple()
   ).collect::<Vec<_>>();
-  let p1 = blueprints.par_iter().enumerate().map(|(i, &b)| (i as i32 + 1) * max_geodes(b, 24)).sum();
+  let p1 = blueprints.par_iter().enumerate().map(|(i,&b)| max_geodes(b, 24) * (i as u32 + 1)).sum();
   let p2 = blueprints.par_iter().take(3).map(|&b| max_geodes(b, 32)).product();
   (p1,p2)
 }
