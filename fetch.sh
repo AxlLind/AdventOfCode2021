422ffc29c32981ab28549082556a9e2dc1a5c112
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2022-12-27 18:36:08 +0100

    Clean up fetch script

--- fetch.sh
+++ fetch.sh
@@ -3,18 +3,18 @@ set -euo pipefail
 SCRIPT_DIR=$(realpath "$(dirname "$0")")
 
 if [[ $# != 1 ]]; then
-  echo "Please provide a day number."
-  echo "usage: $0 DAY"
+  echo Please provide a day number.
+  echo usage: "$0" DAY
   exit 1
 fi
 
 if [[ ! "$1" =~ ^(0[1-9]|1[0-9]|2[0-5])$ ]]; then
-  echo "Argument '$1' is not a valid day."
+  echo Not a valid day: "$1"
   exit 1
 fi
 
 if [[ -z "${AOC_SESSION-""}" ]]; then
-  echo "No session token set in \$AOC_SESSION."
+  echo \$AOC_SESSION not set
   exit 1
 fi
 
@@ -22,5 +22,5 @@ mkdir -p "$SCRIPT_DIR/inputs"
 
 curl -s "https://adventofcode.com/2022/day/${1#0}/input" \
     --cookie "session=$AOC_SESSION" \
-    -A "Bash script at $(git remote -v | head -n1 | awk '{print $2}')" \
+    -A "Bash script at $(git remote -v | awk 'NR==1{print $2}')" \
     | tee "$SCRIPT_DIR/inputs/$1.in"
