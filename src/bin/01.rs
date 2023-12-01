fd5973df2c5b9558e71715bb786eedcdf4133fd1
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2023-12-01 07:24:01 +0100

    Simplify match expression

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,16 +1,12 @@
 const STR_DIGITS: &[&[u8]] = &[b"one", b"two", b"three", b"four", b"five", b"six", b"seven", b"eight", b"nine"];
 
 fn digit_sum(w: &[u8], part_two: bool) -> usize {
-  let mut digits = (0..w.len()).filter_map(|i| {
-    if (b'0'..=b'9').contains(&w[i]) {
-      return Some((w[i] - b'0') as usize);
-    }
-    if !part_two {
-      return None
-    }
-    STR_DIGITS.iter()
+  let mut digits = (0..w.len()).filter_map(|i| match w[i] {
+    b'0'..=b'9' => Some((w[i] - b'0') as usize),
+    _ if part_two => STR_DIGITS.iter()
       .enumerate()
-      .find_map(|(di, d)| w[i..].starts_with(d).then_some(di + 1))
+      .find_map(|(di, d)| w[i..].starts_with(d).then_some(di + 1)),
+    _ => None
   });
   let a = digits.next().unwrap();
   let b = digits.last().unwrap_or(a);
