e337725cb7cf4224e5ae2fc8c477550a88ba77b1
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2023-12-01 06:53:29 +0100

    Split out into function per word

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,28 +1,26 @@
 const STR_DIGITS: &[&[u8]] = &[b"one", b"two", b"three", b"four", b"five", b"six", b"seven", b"eight", b"nine"];
 
-fn get_digit_sum(lines: &[&[u8]], part_two: bool) -> usize {
-  lines.iter().map(|l| {
-    let mut digits = (0..l.len()).filter_map(|i| {
-      if (b'0'..=b'9').contains(&l[i]) {
-        return Some((l[i] - b'0') as usize);
-      }
-      if !part_two {
-        return None
-      }
-      STR_DIGITS.iter()
-        .enumerate()
-        .find_map(|(di, d)| l[i..].starts_with(d).then_some(di + 1))
-    });
-    let a = digits.next().unwrap();
-    let b = digits.last().unwrap_or(a);
-    a * 10 + b
-  }).sum()
+fn digit_sum(w: &[u8], part_two: bool) -> usize {
+  let mut digits = (0..w.len()).filter_map(|i| {
+    if (b'0'..=b'9').contains(&w[i]) {
+      return Some((w[i] - b'0') as usize);
+    }
+    if !part_two {
+      return None
+    }
+    STR_DIGITS.iter()
+      .enumerate()
+      .find_map(|(di, d)| w[i..].starts_with(d).then_some(di + 1))
+  });
+  let a = digits.next().unwrap();
+  let b = digits.last().unwrap_or(a);
+  a * 10 + b
 }
 
 #[aoc::main(01)]
 fn main(input: &str) -> (usize, usize) {
   let lines = input.split('\n').map(str::as_bytes).collect::<Vec<_>>();
-  let p1 = get_digit_sum(&lines, false);
-  let p2 = get_digit_sum(&lines, true);
+  let p1 = lines.iter().map(|line| digit_sum(line, false)).sum();
+  let p2 = lines.iter().map(|line| digit_sum(line, true)).sum();
   (p1, p2)
 }
