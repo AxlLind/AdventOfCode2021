226f822f11fa6f9faa08bf1b42f46fc9b4a1c1a0
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2023-12-02 10:19:52 +0100

    Flatten nested loop

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,16 +1,14 @@
 fn parse_game(line: &str) -> Option<(usize, usize)> {
-  let (game_id, rest) = line.trim_start_matches("Game ").split_once(':')?;
+  let (game_id, game) = line.trim_start_matches("Game ").split_once(':')?;
   let (mut r, mut g, mut b, mut possible) = (0, 0, 0, true);
-  for game in rest.split(';') {
-    for s in game.split(',') {
-      let (n, color) = s.trim().split_once(' ')?;
-      let n = n.parse().ok()?;
-      match color.as_bytes()[0] {
-        b'r' => {possible &= n <= 12; r = r.max(n)},
-        b'g' => {possible &= n <= 13; g = g.max(n)},
-        b'b' => {possible &= n <= 14; b = b.max(n)},
-        _ => unreachable!(),
-      }
+  for action in game.split(';').flat_map(|s| s.split(',')) {
+    let (n, color) = action.trim().split_once(' ')?;
+    let n = n.parse().ok()?;
+    match color.as_bytes()[0] {
+      b'r' => {possible &= n <= 12; r = r.max(n)},
+      b'g' => {possible &= n <= 13; g = g.max(n)},
+      b'b' => {possible &= n <= 14; b = b.max(n)},
+      _ => unreachable!(),
     }
   }
   Some((if possible {game_id.parse().ok()?} else {0}, r * g * b))
