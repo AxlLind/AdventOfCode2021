182b9c7da8d416a75e0b32a4453ceabc26c29c90
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2023-12-03 19:43:21 +0100

    Break symbol lookup into function

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -1,5 +1,16 @@
 use hashbrown::HashMap;
 
+fn find_symbol(lines: &[&[u8]], r: usize, c: usize) -> Option<(usize, usize, char)> {
+  for (dr, dc) in [(-1,-1),(-1,0),(-1,1),(0,-1),(0,1),(1,-1),(1,0),(1,1)] {
+    let (rr, cc) = ((r as i32 + dr) as usize, (c as i32 + dc) as usize);
+    let Some(&s) = lines.get(rr).and_then(|l| l.get(cc)) else {continue};
+    if s != b'.' && !s.is_ascii_digit() {
+      return Some((cc, rr, s as char));
+    }
+  }
+  None
+}
+
 #[aoc::main(03)]
 fn main(input: &str) -> (usize, usize) {
   let lines = input.split('\n').map(str::as_bytes).collect::<Vec<_>>();
@@ -9,21 +20,12 @@ fn main(input: &str) -> (usize, usize) {
     while c < l.len() {
       let (start, mut symbol) = (c, None);
       while c < l.len() && l[c].is_ascii_digit() {
-        for (dr, dc) in [(-1,-1),(-1,0),(-1,1),(0,-1),(0,1),(1,-1),(1,0),(1,1)] {
-          let (rr, cc) = ((r as i32 + dr) as usize, (c as i32 + dc) as usize);
-          let Some(&s) = lines.get(rr).and_then(|l| l.get(cc)) else {continue};
-          if s != b'.' && !s.is_ascii_digit() {
-            symbol = Some((cc, rr, s as char));
-            break;
-          }
-        }
+        symbol = symbol.or_else(|| find_symbol(&lines, r, c));
         c += 1;
       }
-      if start < c {
-        if let Some(symbol) = symbol {
-          let num = l[start..c].iter().fold(0, |n, c| n * 10 + (c - b'0') as usize);
-          symbols.entry(symbol).or_insert(Vec::new()).push(num);
-        }
+      if let Some(symbol) = symbol {
+        let num = l[start..c].iter().fold(0, |n, c| n * 10 + (c - b'0') as usize);
+        symbols.entry(symbol).or_insert(Vec::new()).push(num);
       }
       c += 1;
     }
