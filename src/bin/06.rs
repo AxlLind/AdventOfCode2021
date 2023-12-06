ffe87128654593337c36a190fb16b1d6dcec4122
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2023-12-06 07:11:01 +0100

    Simplify whole day 06

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -1,25 +1,18 @@
-use itertools::Itertools;
-
-// d = (t - w) * w
-// => w = (t +- sqrt(t^2 - 4 d)) / 2
-fn calc(times: &[usize], dists: &[usize]) -> usize {
-  times.iter().zip(dists).map(|(&t, &d)| {
-    let diff = ((t*t - 4*d) as f64).sqrt();
-    let l = (t as f64 - diff) / 2.0;
-    let h = (t as f64 + diff) / 2.0;
-    (h.floor() - l.ceil()) as usize + 1
-  }).product()
+// d = (t - w) * w  =>  w = (t +- sqrt(t^2 - 4d)) / 2
+fn f(t: usize, d: usize) -> usize {
+  let diff = ((t*t - 4*d) as f64).sqrt();
+  let l = (t as f64 - diff) / 2.0;
+  let h = (t as f64 + diff) / 2.0;
+  (h.floor() - l.ceil()) as usize + 1
 }
 
 #[aoc::main(06)]
 fn main(input: &str) -> (usize, usize) {
-  let (times, dists) = input.lines().map(|l|
-    l.split_whitespace()
-      .skip(1)
-      .map(|w| w.parse::<usize>().unwrap())
-      .collect::<Vec<_>>()
-  ).collect_tuple().unwrap();
-  let p2time = times.iter().map(|w| w.to_string()).collect::<String>().parse().unwrap();
-  let p2dist = dists.iter().map(|w| w.to_string()).collect::<String>().parse().unwrap();
-  (calc(&times, &dists), calc(&[p2time], &[p2dist]))
+  let (l1, l2) = input.split_once('\n').unwrap();
+  let times = l1.split_whitespace().skip(1).map(|w| w.parse().unwrap()).collect::<Vec<_>>();
+  let dists = l2.split_whitespace().skip(1).map(|w| w.parse().unwrap()).collect::<Vec<_>>();
+  let time2 = l1.split_whitespace().skip(1).collect::<String>().parse().unwrap();
+  let dist2 = l2.split_whitespace().skip(1).collect::<String>().parse().unwrap();
+  let p1 = times.iter().zip(dists).map(|(&t, d)| f(t, d)).product();
+  (p1, f(time2, dist2))
 }
