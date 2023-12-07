4d54a92a96331086b6eded6bc0df2b32b32ee4ce
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2023-12-07 07:34:30 +0100

    Optimize 07 by caching sort keys

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -1,4 +1,3 @@
-use std::cmp::Ordering;
 use itertools::Itertools;
 
 fn card_index(c: char, p2: bool) -> usize {
@@ -20,14 +19,14 @@ fn card_index(c: char, p2: bool) -> usize {
   }
 }
 
-fn card_type(cards: &str, p2: bool) -> usize {
+fn card_key(cards: &str, p2: bool) -> (usize, usize) {
   let counts_by_card = cards.chars().counts();
   let counts = counts_by_card.iter()
     .filter(|&(k,_)| *k != 'J' || !p2)
     .map(|(_,v)| *v)
     .collect::<Vec<_>>();
   let jokers = if p2 {*counts_by_card.get(&'J').unwrap_or(&0)} else {0};
-  match (*counts.iter().max().unwrap_or(&0), jokers) {
+  let card_type = match (*counts.iter().max().unwrap_or(&0), jokers) {
     (a,b) if a + b == 5 => 6,
     (a,b) if a + b == 4 => 5,
     (3,0) => if counts.contains(&2) {4} else {3},
@@ -43,32 +42,22 @@ fn card_type(cards: &str, p2: bool) -> usize {
     (1,2) => 3,
     (1,1) => 1,
     _ => 0,
-  }
-}
-
-fn comp_cards(c1: &str, c2: &str, p2: bool) -> Ordering {
-  match card_type(c1, p2).cmp(&card_type(c2, p2)) {
-    Ordering::Equal => {
-      c1.chars().zip(c2.chars()).find_map(|(a,b)| {
-        match card_index(a, p2).cmp(&card_index(b, p2)) {
-          Ordering::Equal => None,
-          ord => Some(ord),
-        }
-      }).unwrap()
-    },
-    ord => ord,
-  }
+  };
+  let idx = cards.chars().fold(0, |acc, c| (acc << 4) + card_index(c, p2));
+  (card_type, idx)
 }
 
 #[aoc::main(07)]
 fn main(input: &str) -> (usize, usize) {
   let mut cards = input.split('\n').map(|l| {
-    let (cards, n) = l.split_once(' ').unwrap();
-    (cards, n.parse().unwrap())
+    let (cards, bid) = l.split_once(' ').unwrap();
+    let p1key = card_key(cards, false);
+    let p2key = card_key(cards, true);
+    (cards, bid.parse().unwrap(), p1key, p2key)
   }).collect::<Vec<_>>();
-  cards.sort_by(|a, b| comp_cards(a.0, b.0, false));
-  let p1 = cards.iter().enumerate().map(|(rank, (_, bid))| (rank + 1) * bid).sum();
-  cards.sort_by(|a, b| comp_cards(a.0, b.0, true));
-  let p2 = cards.iter().enumerate().map(|(rank, (_, bid))| (rank + 1) * bid).sum();
+  cards.sort_by_key(|&(_,_,key,_)| key);
+  let p1 = cards.iter().enumerate().map(|(rank, (_,bid,_,_))| (rank + 1) * bid).sum();
+  cards.sort_by_key(|&(_,_,_,key)| key);
+  let p2 = cards.iter().enumerate().map(|(rank, (_,bid,_,_))| (rank + 1) * bid).sum();
   (p1, p2)
 }
