7f3fe573a37331b6e250418dc9eb0777943aede8
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2023-12-09 06:32:50 +0100

    Do fold directly

--- src/bin/09.rs
+++ src/bin/09.rs
@@ -2,7 +2,7 @@ use itertools::Itertools;
 
 #[aoc::main(09)]
 fn main(input: &str) -> (isize, isize) {
-  input.split('\n').map(|l| {
+  input.split('\n').fold((0,0), |(p1, p2), l| {
     let xs = l.split_whitespace()
       .map(|w| w.parse::<isize>().unwrap())
       .collect::<Vec<_>>();
@@ -14,6 +14,7 @@ fn main(input: &str) -> (isize, isize) {
         .collect();
       v.push(xs);
     }
-    v.iter().rev().fold((0,0), |(p1, p2), xs| (xs[xs.len()-1] + p1, xs[0] - p2))
-  }).fold((0, 0), |(a,b),(x,y)| (a+x, b+y))
+    let (a,b) = v.iter().rev().fold((0,0), |(a,b), xs| (xs[xs.len()-1]+a, xs[0]-b));
+    (p1 + a, p2 + b)
+  })
 }
