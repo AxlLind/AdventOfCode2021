90ee9b1c244977a4283f445f9acc7b4b5330e481
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2023-12-11 06:46:32 +0100

    Simplify day 11

--- src/bin/11.rs
+++ src/bin/11.rs
@@ -1,16 +1,15 @@
 use itertools::Itertools;
 
 fn solve(universe: &Vec<Vec<u8>>, mut galaxies: Vec<(usize, usize)>, size: usize) -> usize {
-  let empty_rows = (0..universe.len()).rev()
-    .filter(|&r| universe[r].iter().all(|&c| c == b'.'));
-  let empty_cols = (0..universe[0].len()).rev()
-    .filter(|&c| (0..universe.len()).all(|r| universe[r][c] == b'.'));
-  for r in empty_rows {
+  let (rows, cols) = (universe.len(), universe[0].len());
+  let empty_rows = (0..rows).filter(|&r| universe[r].iter().all(|&c| c == b'.'));
+  let empty_cols = (0..cols).filter(|&c| (0..rows).all(|r| universe[r][c] == b'.'));
+  for r in empty_rows.rev() {
     for g in &mut galaxies {
       if g.0 > r { g.0 += size - 1 }
     }
   }
-  for c in empty_cols {
+  for c in empty_cols.rev() {
     for g in &mut galaxies {
       if g.1 > c { g.1 += size - 1 }
     }
