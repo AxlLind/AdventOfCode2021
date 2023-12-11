2c4cfddb9490c4174e369d2e7b06302cae5e1cc7
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2023-12-11 06:38:44 +0100

    Simplify day 11

--- src/bin/11.rs
+++ src/bin/11.rs
@@ -1,24 +1,18 @@
 use itertools::Itertools;
 
-fn solve(universe: &Vec<String>, mut galaxies: Vec<(usize, usize)>, size: usize) -> usize {
-  for r in (0..universe.len()).rev() {
-    if universe[r].bytes().any(|c| c != b'.') {
-      continue;
-    }
+fn solve(universe: &Vec<Vec<u8>>, mut galaxies: Vec<(usize, usize)>, size: usize) -> usize {
+  let empty_rows = (0..universe.len()).rev()
+    .filter(|&r| universe[r].iter().all(|&c| c == b'.'));
+  let empty_cols = (0..universe[0].len()).rev()
+    .filter(|&c| (0..universe.len()).all(|r| universe[r][c] == b'.'));
+  for r in empty_rows {
     for g in &mut galaxies {
-      if g.0 > r {
-        g.0 += size - 1;
-      }
+      if g.0 > r { g.0 += size - 1 }
     }
   }
-  for c in (0..universe[0].len()).rev() {
-    if (0..universe.len()).any(|r| universe[r].as_bytes()[c] != b'.') {
-      continue;
-    }
+  for c in empty_cols {
     for g in &mut galaxies {
-      if g.1 > c {
-        g.1 += size - 1;
-      }
+      if g.1 > c { g.1 += size - 1 }
     }
   }
   galaxies.iter()
@@ -29,10 +23,12 @@ fn solve(universe: &Vec<String>, mut galaxies: Vec<(usize, usize)>, size: usize)
 
 #[aoc::main(11)]
 fn main(input: &str) -> (usize, usize) {
-  let universe = input.split('\n').map(|l| l.to_string()).collect::<Vec<_>>();
+  let universe = input.split('\n')
+    .map(|l| l.as_bytes().to_vec())
+    .collect::<Vec<_>>();
   let galaxies = (0..universe.len())
     .cartesian_product(0..universe[0].len())
-    .filter(|(r,c)| universe[*r].as_bytes()[*c] == b'#')
+    .filter(|&(r,c)| universe[r][c] == b'#')
     .collect::<Vec<_>>();
   let p1 = solve(&universe, galaxies.clone(), 2);
   let p2 = solve(&universe, galaxies, 1000000);
