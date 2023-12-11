acf1666aed1605cd7ad39d98ba114d5c00302709
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2023-12-11 06:28:39 +0100

    Finish day 11

---
+++ src/bin/11.rs
@@ -0,0 +1,40 @@
+use itertools::Itertools;
+
+fn solve(universe: &Vec<String>, mut galaxies: Vec<(usize, usize)>, size: usize) -> usize {
+  for r in (0..universe.len()).rev() {
+    if universe[r].bytes().any(|c| c != b'.') {
+      continue;
+    }
+    for g in &mut galaxies {
+      if g.0 > r {
+        g.0 += size - 1;
+      }
+    }
+  }
+  for c in (0..universe[0].len()).rev() {
+    if (0..universe.len()).any(|r| universe[r].as_bytes()[c] != b'.') {
+      continue;
+    }
+    for g in &mut galaxies {
+      if g.1 > c {
+        g.1 += size - 1;
+      }
+    }
+  }
+  galaxies.iter()
+    .tuple_combinations()
+    .map(|(&(r1,c1), &(r2,c2))| r1.abs_diff(r2) + c1.abs_diff(c2))
+    .sum()
+}
+
+#[aoc::main(11)]
+fn main(input: &str) -> (usize, usize) {
+  let universe = input.split('\n').map(|l| l.to_string()).collect::<Vec<_>>();
+  let galaxies = (0..universe.len())
+    .cartesian_product(0..universe[0].len())
+    .filter(|(r,c)| universe[*r].as_bytes()[*c] == b'#')
+    .collect::<Vec<_>>();
+  let p1 = solve(&universe, galaxies.clone(), 2);
+  let p2 = solve(&universe, galaxies, 1000000);
+  (p1, p2)
+}
