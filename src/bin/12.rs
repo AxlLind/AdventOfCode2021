07ab09f3f1d52321839ad91a4496d9d1c33b5934
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2023-12-12 07:10:26 +0100

    Improve cache performance with smaller keys

--- src/bin/12.rs
+++ src/bin/12.rs
@@ -1,23 +1,21 @@
 use hashbrown::HashMap;
 use itertools::Itertools;
 
-fn possible_ways<'a>(cache: &mut HashMap<(&'a [u8], Option<usize>, &'a [usize]), usize>, s: &'a [u8], within: Option<usize>, remaining: &'a [usize]) -> usize {
-  fn dp<'a>(cache: &mut HashMap<(&'a [u8], Option<usize>, &'a [usize]), usize>, s: &'a [u8], within: Option<usize>, remaining: &'a [usize]) -> usize {
+fn possible_ways(cache: &mut HashMap<(usize, usize, usize), usize>, s: &[u8], within: Option<usize>, remaining: &[usize]) -> usize {
+  fn dp(cache: &mut HashMap<(usize, usize, usize), usize>, s: &[u8], within: Option<usize>, remaining: &[usize]) -> usize {
     if s.is_empty() {
-      if within.is_none() && remaining.is_empty() {
-        return 1;
-      }
-      if remaining.len() == 1 && within == Some(remaining[0]) {
-        return 1;
-      }
-      return 0;
+      return match (within, remaining.len()) {
+        (None, 0) => 1,
+        (Some(x), 1) if x == remaining[0] => 1,
+        _ => 0
+      };
     }
     let possible = s.iter().filter(|&&c| c == b'#' || c == b'?').count();
     let remaining_sum = remaining.iter().sum();
     if possible + within.unwrap_or(0) < remaining_sum {
       return 0;
     }
-    if within.is_some() && remaining.is_empty() {
+    if within.is_some() && remaining_sum == 0 {
       return 0;
     }
     if s[0] == b'.' && within.is_some_and(|x| x != remaining[0]) {
@@ -42,23 +40,28 @@ fn possible_ways<'a>(cache: &mut HashMap<(&'a [u8], Option<usize>, &'a [usize]),
     possible
   }
 
-  if let Some(&x) = cache.get(&(s, within, remaining)) {
+  let key = (s.len(), within.unwrap_or(0), remaining.len());
+  if let Some(&x) = cache.get(&key) {
     return x;
   }
   let possible = dp(cache, s, within, remaining);
-  cache.insert((s, within, remaining), possible);
+  cache.insert(key, possible);
   possible
 }
 
 #[aoc::main(12)]
 fn main(input: &str) -> (usize, usize) {
+  let mut cache_p1 = HashMap::new();
+  let mut cache_p2 = HashMap::new();
   input.split('\n').map(|l| {
     let (vents, rest) = l.split_once(' ').unwrap();
     let nums = rest.split(',').map(|w| w.parse::<usize>().unwrap()).collect::<Vec<_>>();
-    let p1 = possible_ways(&mut HashMap::new(), vents.as_bytes(), None, &nums);
+    cache_p1.clear();
+    cache_p2.clear();
+    let p1 = possible_ways(&mut cache_p1, vents.as_bytes(), None, &nums);
     let new_vents = (0..5).map(|_| vents).join("?");
     let new_nums = (0..5).flat_map(|_| &nums).copied().collect::<Vec<_>>();
-    let p2 = possible_ways(&mut HashMap::new(), new_vents.as_bytes(), None, &new_nums);
+    let p2 = possible_ways(&mut cache_p2, new_vents.as_bytes(), None, &new_nums);
     (p1,p2)
   }).fold((0,0), |(p1,p2), (a,b)| (p1+a, p2+b))
 }
