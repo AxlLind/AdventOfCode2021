4c991381c93ca5beb0d04b8d26185efc7aef5ce6
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2023-12-15 17:09:28 +0100

    Remove unnecesary as_bytes()

--- src/bin/15.rs
+++ src/bin/15.rs
@@ -1,5 +1,5 @@
-fn hash(s: &[u8]) -> usize {
-  s.iter().fold(0, |a, c| (a + c) * 17) as usize
+fn hash(s: &str) -> usize {
+  s.bytes().fold(0, |a, c| (a + c) * 17) as usize
 }
 
 #[aoc::main(15)]
@@ -7,10 +7,10 @@ fn main(input: &str) -> (usize, usize) {
   let mut map = vec![Vec::new(); 256];
   let mut p1 = 0;
   for w in input.split(',') {
-    p1 += hash(w.as_bytes());
+    p1 += hash(w);
     let label_end = w.bytes().position(|c| c == b'-' || c == b'=').unwrap();
     let label = &w[..label_end];
-    let bucket = &mut map[hash(label.as_bytes())];
+    let bucket = &mut map[hash(label)];
     match (w.as_bytes()[label_end], bucket.iter().position(|&(l,_)| l == label)) {
       (b'=', Some(i)) => bucket[i] = (label, w[label_end+1..].parse::<usize>().unwrap()),
       (b'=', None)    => bucket.push((label, w[label_end+1..].parse::<usize>().unwrap())),
