e9b698eff7266ac104c7dd101dc3f11a55563777
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2023-12-16 10:48:39 +0100

    Simplify day 16

--- src/bin/16.rs
+++ src/bin/16.rs
@@ -1,51 +1,43 @@
-use hashbrown::HashSet;
-use itertools::Itertools;
-
-const DW: [(isize, isize); 4] = [
-  (-1, 0),
-  ( 0, 1),
-  ( 1, 0),
-  ( 0,-1),
-];
+use hashbrown::HashMap;
 
 fn step(r: usize, c: usize, d: usize) -> (usize, usize, usize) {
-  let (dr, dc) = DW[d];
-  ((r as isize + dr) as usize, (c as isize + dc) as usize, d)
+  let (dr, dc) = [(-1,0),(0,1),(1,0),(0,-1)][d];
+  ((r as isize + dr) as _, (c as isize + dc) as _, d)
 }
 
 fn energized_tiles(grid: &[&[u8]], start: (usize,usize,usize)) -> usize {
-  let mut seen = HashSet::new();
+  let mut seen = HashMap::<_,[_;4]>::new();
   let mut beams = vec![start];
   while !beams.is_empty() {
-    let mut new_beams = Vec::new();
+    let mut new_beams = Vec::with_capacity(beams.capacity());
     for (r,c,d) in beams {
       if r >= grid.len() || c >= grid[0].len() {
         continue;
       }
-      if !seen.insert((r,c,d)) {
+      let seen_dir = seen.entry((r,c)).or_default();
+      if seen_dir[d] {
         continue;
       }
+      seen_dir[d] = true;
       match (grid[r][c], d) {
         (b'.',   _) => new_beams.push(step(r,c,d)),
         (b'/',   _) => new_beams.push(step(r,c,[1,0,3,2][d])),
         (b'\\',  _) => new_beams.push(step(r,c,[3,2,1,0][d])),
         (b'|', 0|2) => new_beams.push(step(r,c,d)),
-        (b'|',   _) => new_beams.extend([step(r,c,0), step(r,c,2)]),
         (b'-', 1|3) => new_beams.push(step(r,c,d)),
+        (b'|',   _) => new_beams.extend([step(r,c,0), step(r,c,2)]),
         (b'-',   _) => new_beams.extend([step(r,c,1), step(r,c,3)]),
         _ => unreachable!(),
       }
     }
     beams = new_beams;
   }
-  seen.iter().map(|&(r,c,_)| (r,c)).unique().count()
+  seen.len()
 }
 
 #[aoc::main(16)]
 fn main(input: &str) -> (usize, usize) {
-  let grid = input.split('\n').map(|l| {
-    l.as_bytes()
-  }).collect::<Vec<_>>();
+  let grid = input.split('\n').map(str::as_bytes).collect::<Vec<_>>();
   let p2 = (0..grid.len()).flat_map(|r| [(r,0,1), (r,grid[0].len()-1,3)])
     .chain((0..grid[0].len()).flat_map(|c| [(0,c,2), (grid.len()-1,c,0)]))
     .map(|start| energized_tiles(&grid, start))
