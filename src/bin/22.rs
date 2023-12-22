5fae8066326c3c0913e25aabb6be4539df42855f
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2023-12-22 09:35:35 +0100

    Build adjacent list while falling the bricks

--- src/bin/22.rs
+++ src/bin/22.rs
@@ -22,6 +22,8 @@ fn main(input: &str) -> (usize, usize) {
     (x1, y1, z1, x2, y2, z2)
   }).collect::<Vec<_>>();
   bricks.sort_by_key(|b| b.2);
+
+  let mut adjacent = vec![(HashSet::new(), HashSet::new()); bricks.len()];
   let mut space = HashMap::new();
   for (i, b) in bricks.iter_mut().enumerate() {
     let (x1, y1, mut z1, x2, y2, mut z2) = *b;
@@ -29,21 +31,20 @@ fn main(input: &str) -> (usize, usize) {
       z2 -= 1;
       z1 -= 1;
     }
-    let positions = (x1..=x2).cartesian_product(y1..=y2).cartesian_product(z1..=z2);
-    space.extend(positions.map(|((x,y),z)| ((x,y,z),i)));
-    *b = (x1, y1, z1, x2, y2, z2);
-  }
-  let mut adjacent = vec![(HashSet::new(), HashSet::new()); bricks.len()];
-  for (i, &(x1, y1, z1, x2, y2, _)) in bricks.iter().enumerate() {
     for (x,y) in (x1..=x2).cartesian_product(y1..=y2) {
+      for z in z1..=z2 {
+        space.insert((x,y,z), i);
+      }
       if let Some(&j) = space.get(&(x,y,z1-1)) {
         adjacent[j].0.insert(i);
         adjacent[i].1.insert(j);
       }
     }
+    *b = (x1, y1, z1, x2, y2, z2);
   }
-  let (mut p1, mut p2) = (0, 0);
+
   let mut falling = HashSet::new();
+  let (mut p1, mut p2) = (0, 0);
   for b in 0..bricks.len() {
     falling.clear();
     disintegrate_all(&adjacent, &mut falling, b);
