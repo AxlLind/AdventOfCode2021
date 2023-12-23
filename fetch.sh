821c49f6b4ff4a617f07c58e9b3c1f3cb879633a
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2023-12-23 10:49:18 +0100

    Use quotes in echo

--- fetch.sh
+++ fetch.sh
@@ -3,18 +3,18 @@ set -euo pipefail
 SCRIPT_DIR=$(realpath "$(dirname "$0")")
 
 if [[ $# != 1 ]]; then
-  echo Please provide a day number.
-  echo usage: "$0" DAY
+  echo "Please provide a day number."
+  echo "usage: $0 DAY"
   exit 1
 fi
 
 if [[ ! "$1" =~ ^(0[1-9]|1[0-9]|2[0-5])$ ]]; then
-  echo Not a valid day: "$1"
+  echo "Not a valid day: $1"
   exit 1
 fi
 
 if [[ -z "${AOC_SESSION-""}" ]]; then
-  echo \$AOC_SESSION not set
+  echo "\$AOC_SESSION not set"
   exit 1
 fi
 
