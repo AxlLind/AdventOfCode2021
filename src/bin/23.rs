3f23f300163a35471990bdb811adbb3edc7f6f71
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2023-12-23 07:52:48 +0100

    Simplify loop in 23

--- src/bin/23.rs
+++ src/bin/23.rs
@@ -27,7 +27,7 @@ fn solve(grid: &[&[u8]], part2: bool) -> usize {
       if grid[r][c] == b'#' {
         continue;
       }
-      let neighbours: &[_] = match grid[r][c] {
+      let neighbors: &[_] = match grid[r][c] {
         _ if part2 => &[(-1,0),(1,0),(0,-1),(0,1)],
         b'.' => &[(-1,0),(1,0),(0,-1),(0,1)],
         b'^' => &[(-1,0)],
@@ -37,7 +37,7 @@ fn solve(grid: &[&[u8]], part2: bool) -> usize {
         _ => unreachable!(),
       };
       let e = graph.entry((r,c)).or_default();
-      for (dr, dc) in neighbours {
+      for (dr, dc) in neighbors {
         let rr = (r as isize + dr) as usize;
         let cc = (c as isize + dc) as usize;
         let Some(&tile) = grid.get(rr).and_then(|row| row.get(cc)) else {continue};
@@ -47,11 +47,10 @@ fn solve(grid: &[&[u8]], part2: bool) -> usize {
       }
     }
   }
-  loop {
-    let Some((&(r,c), _)) = graph.iter().find(|(_,n)| n.len() == 2) else { break };
-    let neighbours = graph.remove(&(r,c)).unwrap();
-    let (r1,c1,d1) = neighbours[0];
-    let (r2,c2,d2) = neighbours[1];
+  while let Some((&(r,c), _)) = graph.iter().find(|(_,n)| n.len() == 2) {
+    let neighbors = graph.remove(&(r,c)).unwrap();
+    let (r1,c1,d1) = neighbors[0];
+    let (r2,c2,d2) = neighbors[1];
     let n1 = graph.entry((r1,c1)).or_default();
     if let Some(i) = n1.iter().position(|&(rr,cc,_)| (rr,cc) == (r,c)) {
       n1[i] = (r2,c2,d1+d2);
