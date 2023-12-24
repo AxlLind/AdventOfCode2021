e7bdadfadd9f403bfb93938c9159afb8742d066b
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2023-12-24 07:21:23 +0100

    Remove unnecessary functions

--- src/bin/24.py
+++ src/bin/24.py
@@ -3,28 +3,23 @@ from z3 import Int, Solver
 
 # TODO: Port this to Rust somehow??
 
-def parse(inp):
-    lines = []
-    for l in inp.splitlines():
-        a, b = l.split(' @ ')
-        x, y, z = [int(w) for w in a.split(', ')]
-        dx, dy, dz = [int(w) for w in b.split(', ')]
-        lines.append(((x,y,z),(dx,dy,dz)))
-    return lines
-
-def part2(lines):
-    fx,  fy,  fz  = Int("fx"),  Int("fy"),  Int("fz")
-    fdx, fdy, fdz = Int("fdx"), Int("fdy"), Int("fdz")
-    s = Solver()
-    for i, ((x,y,z), (dx,dy,dz)) in enumerate(lines):
-        t = Int(f"t{i}")
-        s.add(t >= 0)
-        s.add(x + dx * t == fx + fdx * t)
-        s.add(y + dy * t == fy + fdy * t)
-        s.add(z + dz * t == fz + fdz * t)
-    assert str(s.check()) == 'sat'
-    return s.model().eval(fx + fy + fz)
-
 inp = Path("./inputs/24.in").read_text().strip()
-lines = parse(inp)
-print(part2(lines))
+lines = []
+for l in inp.splitlines():
+    a, b = l.split(' @ ')
+    x, y, z = [int(w) for w in a.split(', ')]
+    dx, dy, dz = [int(w) for w in b.split(', ')]
+    lines.append(([x,y,z], [dx,dy,dz]))
+
+fx,  fy,  fz  = Int("fx"),  Int("fy"),  Int("fz")
+fdx, fdy, fdz = Int("fdx"), Int("fdy"), Int("fdz")
+s = Solver()
+for i, ((x,y,z), (dx,dy,dz)) in enumerate(lines):
+    t = Int(f"t{i}")
+    s.add(t >= 0)
+    s.add(x + dx * t == fx + fdx * t)
+    s.add(y + dy * t == fy + fdy * t)
+    s.add(z + dz * t == fz + fdz * t)
+assert str(s.check()) == 'sat'
+
+print(s.model().eval(fx + fy + fz))
