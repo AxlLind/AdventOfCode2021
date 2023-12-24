98533ccaa7e3d81da43a0e7353d8795b9e307e36
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2023-12-24 09:57:40 +0100

    Clean up z3 solution

--- src/bin/24.rs
+++ src/bin/24.rs
@@ -12,7 +12,8 @@ fn intersection((x1,y1,dx1,dy1): (f64,f64,f64,f64), (x2,y2,dx2,dy2): (f64,f64,f6
   Some((x,y))
 }
 
-fn find_intersections(lines: &[((f64,f64,f64),(f64,f64,f64))], start: f64, end: f64) -> usize {
+fn find_intersections(lines: &[((f64,f64,f64),(f64,f64,f64))]) -> usize {
+  let range = 200000000000000.0..=400000000000000.0;
   lines.iter()
     .tuple_combinations()
     .filter(|(&((x1,y1,_),(dx1,dy1,_)), &((x2,y2,_),(dx2,dy2,_)))| {
@@ -20,23 +21,21 @@ fn find_intersections(lines: &[((f64,f64,f64),(f64,f64,f64))], start: f64, end: 
       if dx1.signum() != (x-x1).signum() || dx2.signum() != (x-x2).signum() {
         return false;
       }
-      (start..=end).contains(&x) && (start..=end).contains(&y)
+      range.contains(&x) && range.contains(&y)
     })
     .count()
 }
 
 fn part2(lines: &[((f64,f64,f64),(f64,f64,f64))]) -> i64 {
-  let cfg = z3::Config::new();
-  let ctx = z3::Context::new(&cfg);
+  let ctx = z3::Context::new(&z3::Config::new());
   let s = z3::Solver::new(&ctx);
-
-  let (fx,fy,fz)    = (Int::new_const(&ctx, "fx"),  Int::new_const(&ctx, "fy"),  Int::new_const(&ctx, "fz"));
-  let (fdx,fdy,fdz) = (Int::new_const(&ctx, "fdx"), Int::new_const(&ctx, "fdy"), Int::new_const(&ctx, "fdz"));
+  let [fx,fy,fz] = ["fx", "fy", "fz"].map(|v| Int::new_const(&ctx, v));
+  let [fdx,fdy,fdz] = ["fdx", "fdy", "fdz"].map(|v| Int::new_const(&ctx, v));
 
   let zero = Int::from_i64(&ctx, 0);
   for (i, &((x,y,z), (dx,dy,dz))) in lines.iter().enumerate() {
-    let (x,y,z)    = (Int::from_i64(&ctx, x as _),  Int::from_i64(&ctx, y as _),  Int::from_i64(&ctx, z as _));
-    let (dx,dy,dz) = (Int::from_i64(&ctx, dx as _), Int::from_i64(&ctx, dy as _), Int::from_i64(&ctx, dz as _));
+    let [x,y,z] = [x,y,z].map(|v| Int::from_i64(&ctx, v as _));
+    let [dx,dy,dz] = [dx,dy,dz].map(|v| Int::from_i64(&ctx, v as _));
     let t = Int::new_const(&ctx, format!("t{i}"));
     s.assert(&t.ge(&zero));
     s.assert(&((&x + &dx * &t)._eq(&(&fx + &fdx * &t))));
@@ -57,6 +56,5 @@ fn main(input: &str) -> (usize, i64) {
     let (dx,dy,dz) = b.split(", ").map(|w| w.trim().parse::<f64>().unwrap()).collect_tuple().unwrap();
     ((x,y,z), (dx,dy,dz))
   }).collect::<Vec<_>>();
-  let p1 = find_intersections(&lines, 200000000000000.0, 400000000000000.0);
-  (p1, part2(&lines))
+  (find_intersections(&lines), part2(&lines))
 }
