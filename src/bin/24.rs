e0325f3d5efa466f2753eff889e2ea2924660af7
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2023-12-25 10:52:56 +0100

    Clean up 24

--- src/bin/24.rs
+++ src/bin/24.rs
@@ -1,27 +1,21 @@
 use z3::ast::{Ast, Int};
 use itertools::Itertools;
 
-fn intersection((x1,y1,dx1,dy1): (f64,f64,f64,f64), (x2,y2,dx2,dy2): (f64,f64,f64,f64)) -> Option<(f64, f64)> {
-  let m1 = dy1 / dx1;
-  let m2 = dy2 / dx2;
-  if (m2 - m1).abs() < f64::EPSILON {
-    return None;
-  }
-  let x = (m1 * x1 - m2*x2 + y2 - y1) / (m1 - m2);
-  let y = (m1*m2*(x2-x1) + m2*y1 - m1*y2) / (m2 - m1);
-  Some((x,y))
-}
-
-fn find_intersections(lines: &[((f64,f64,f64),(f64,f64,f64))]) -> usize {
-  let range = 200000000000000.0..=400000000000000.0;
+fn part1(lines: &[((f64,f64,f64),(f64,f64,f64))]) -> usize {
   lines.iter()
     .tuple_combinations()
     .filter(|(&((x1,y1,_),(dx1,dy1,_)), &((x2,y2,_),(dx2,dy2,_)))| {
-      let Some((x,y)) = intersection((x1,y1,dx1,dy1), (x2,y2,dx2,dy2)) else { return false };
+      let m1 = dy1 / dx1;
+      let m2 = dy2 / dx2;
+      if (m2 - m1).abs() <= f64::EPSILON {
+        return false;
+      }
+      let x = (m1 * x1 - m2*x2 + y2 - y1) / (m1 - m2);
+      let y = (m1*m2*(x2-x1) + m2*y1 - m1*y2) / (m2 - m1);
       if dx1.signum() != (x-x1).signum() || dx2.signum() != (x-x2).signum() {
         return false;
       }
-      range.contains(&x) && range.contains(&y)
+      [x,y].iter().all(|v| (200000000000000.0..=400000000000000.0).contains(v))
     })
     .count()
 }
@@ -41,8 +35,7 @@ fn part2(lines: &[((f64,f64,f64),(f64,f64,f64))]) -> i64 {
     s.assert(&((&z + &dz * &t)._eq(&(&fz + &fdz * &t))));
   }
   assert_eq!(s.check(), z3::SatResult::Sat);
-  let model = s.get_model().unwrap();
-  let res = model.eval(&(&fx + &fy + &fz), true).unwrap();
+  let res = s.get_model().unwrap().eval(&(&fx + &fy + &fz), true).unwrap();
   res.as_i64().unwrap()
 }
 
@@ -54,5 +47,5 @@ fn main(input: &str) -> (usize, i64) {
     let (dx,dy,dz) = b.split(", ").map(|w| w.trim().parse::<f64>().unwrap()).collect_tuple().unwrap();
     ((x,y,z), (dx,dy,dz))
   }).collect::<Vec<_>>();
-  (find_intersections(&lines), part2(&lines))
+  (part1(&lines), part2(&lines))
 }
