3c39d97a3ec90440a0341296dd9fb46a1b428a1e
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2023-12-27 11:06:21 +0100

    Implement Stoer Wagner min cut algorithm

--- src/bin/25.rs
+++ src/bin/25.rs
@@ -1,43 +1,91 @@
 use hashbrown::{HashMap, HashSet};
-use itertools::Itertools;
 
-fn component_size(graph: &HashMap<&str, HashSet<&str>>, a: &str) -> usize {
-  let (mut seen, mut s) = (HashSet::new(), vec![a]);
-  while let Some(x) = s.pop() {
-    if seen.insert(x) {
-      s.extend(&graph[x]);
+fn maximum_adjacency_search<'a>(
+  g: &HashMap<&'a str, HashMap<&'a str, i64>>,
+  start: &'a str,
+) -> (&'a str, &'a str, i64) {
+  let (mut s, mut t, mut w) = ("", start, 0);
+  let mut candidates = g.keys()
+    .filter(|&&k| k != start)
+    .map(|&k| (k, g.get(start).and_then(|m| m.get(k)).copied().unwrap_or(0)))
+    .collect::<HashMap<_,_>>();
+  while !candidates.is_empty() {
+    let &best_node = candidates.iter().max_by_key(|&(_, &v)| v).unwrap().0;
+    s = t;
+    t = best_node;
+    w = candidates.remove(best_node).unwrap();
+    for (&k, &v) in &g[best_node] {
+      candidates.entry(k).and_modify(|w| *w += v);
     }
   }
-  seen.len()
+  (s, t, w)
+}
+
+fn merge<'a>(
+  g: &mut HashMap<&'a str, HashMap<&'a str, i64>>,
+  s: &'a str,
+  t: &'a str,
+) {
+  for (k, v) in g.remove(t).unwrap() {
+    if k == s {
+      continue;
+    }
+    let m = g.get_mut(k).unwrap();
+    m.remove(t);
+    *m.entry(s).or_default() += v;
+    let m = g.get_mut(s).unwrap();
+    m.remove(t);
+    *m.entry(k).or_default() += v;
+  }
+}
+
+fn reachable(
+  g: &HashMap<&str, HashMap<&str, i64>>,
+  merges: &HashMap<&str, HashSet<&str>>,
+  s: &str,
+  t: &str,
+) -> usize {
+  let mut result = HashSet::<&str>::new();
+  let mut stack = vec![s];
+  while let Some(n) = stack.pop() {
+    result.extend(&merges[n]);
+    let Some(edges) = g.get(n) else { continue };
+    stack.extend(edges.keys().filter(|&&e| !result.contains(e) && e != t));
+  }
+  result.len()
+}
+
+fn min_cut_stoer_wagner<'a>(mut g: HashMap<&'a str, HashMap<&'a str, i64>>) -> usize {
+  let &start = g.iter().next().unwrap().0;
+  let mut best_cut = i64::MAX;
+  let mut best_partition = 0;
+  let mut merges = g.keys()
+    .map(|&k| (k, HashSet::from_iter([k])))
+    .collect::<HashMap<_, _>>();
+  while g.len() > 1 {
+    let (s, t, w) = maximum_adjacency_search(&g, start);
+    if w < best_cut {
+      best_partition = reachable(&g, &merges, s, t);
+      best_cut = w;
+    }
+    merge(&mut g, s, t);
+    let t_set = merges.remove(t).unwrap();
+    merges.entry(s).and_modify(|e| e.extend(t_set));
+  }
+  best_partition
 }
 
 #[aoc::main(25)]
 fn main(input: &str) -> (usize, char) {
-  let mut graph = HashMap::<_,HashSet<_>>::new();
-  let mut edges = HashSet::new();
+  let mut graph = HashMap::<_, HashMap<_,_>>::new();
   for l in input.split('\n') {
     let (a, rest) = l.split_once(": ").unwrap();
     for b in rest.split(' ') {
-      graph.entry(a).or_default().insert(b);
-      graph.entry(b).or_default().insert(a);
-      edges.insert(if a < b {(a,b)} else {(b,a)});
+      graph.entry(a).or_default().insert(b, 1);
+      graph.entry(b).or_default().insert(a, 1);
     }
   }
-
-  let mut dot = String::from("graph {\n");
-  for (a,b) in edges.iter().sorted() {
-    dot += &format!("  {} -- {};\n", a, b);
-  }
-  dot += "}";
-  // Run the following to visualize the graph:
-  //   dot -Tsvg -Kneato out.dot > out.svg
-  // Manually find the three edges.
-  std::fs::write("out.dot", dot).unwrap();
-
-  for (a,b) in [("qdp", "jxx"), ("zbr", "vsx"), ("mlp", "qqq")] {
-    graph.get_mut(a).unwrap().remove(b);
-    graph.get_mut(b).unwrap().remove(a);
-  }
-  let size = component_size(&graph, "qqq");
-  ((graph.len() - size) * size, '🎄')
+  let nodes = graph.len();
+  let size = min_cut_stoer_wagner(graph);
+  ((nodes - size) * size, '🎄')
 }
