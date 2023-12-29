0206a54a7bfaadd9caed44d2ae75070b3684de24
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2023-12-29 11:13:55 +0100

    Simplify day 02

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,34 +1,26 @@
 #[aoc::main(02)]
 fn main(input: &str) -> (usize, usize) {
   let games = input.split('\n').map(|l| {
-    let (game_id, game) = l.trim_start_matches("Game ").split_once(':').unwrap();
+    let (id, game) = l.trim_start_matches("Game ").split_once(':').unwrap();
     let colors = game.split([';', ',']).map(|w| {
       let (n, color) = w.trim().split_once(' ').unwrap();
-      (n.parse().unwrap(), color.as_bytes()[0])
+      let c = "rgb".bytes().position(|c| c == color.as_bytes()[0]).unwrap();
+      (n.parse().unwrap(), c)
     }).collect::<Vec<_>>();
-    (game_id.parse().unwrap(), colors)
+    (id.parse().unwrap(), colors)
   }).collect::<Vec<_>>();
 
-  let p1 = games.iter().filter(|(_, actions)| {
-    actions.iter().all(|&(n,c)| match c {
-      b'r' => n <= 12,
-      b'g' => n <= 13,
-      b'b' => n <= 14,
-      _ => unreachable!(),
-    })
-  }).map(|(id,_)| id).sum();
+  let p1 = games.iter()
+    .filter(|(_, actions)| actions.iter().all(|&(n,c)| n <= [12, 13, 14][c]))
+    .map(|(id,_)| id)
+    .sum();
 
   let p2 = games.iter().map(|(_,actions)| {
-    let (mut r, mut g, mut b) = (0,0,0);
+    let mut a = [0; 3];
     for &(n, c) in actions {
-      match c {
-        b'r' => r = r.max(n),
-        b'g' => g = g.max(n),
-        b'b' => b = b.max(n),
-        _ => unreachable!(),
-      }
+      a[c] = a[c].max(n);
     }
-    r * g * b
+    a[0] * a[1] * a[2]
   }).sum();
 
   (p1, p2)
