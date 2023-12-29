985793f4497784bd805c24b5de95e0795525e990
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2023-12-29 10:05:12 +0100

    Remove unnecessary function

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -1,6 +1,6 @@
 use itertools::Itertools;
 
-fn card_index(c: char, p2: bool) -> usize {
+fn card_idx(c: char, p2: bool) -> usize {
   match c {
     'A' => 14,
     'K' => 13,
@@ -19,8 +19,14 @@ fn card_index(c: char, p2: bool) -> usize {
   }
 }
 
-fn get_hand_type(counts: &[usize], jokers: usize) -> usize {
-  match (*counts.iter().max().unwrap_or(&0), jokers) {
+fn hand_strength(cards: &str, p2: bool) -> (usize, usize) {
+  let card_count = cards.chars().counts();
+  let counts = card_count.iter()
+    .filter(|&(&k,_)| k != 'J' || !p2)
+    .map(|(_,&v)| v)
+    .collect::<Vec<_>>();
+  let jokers = if p2 {*card_count.get(&'J').unwrap_or(&0)} else {0};
+  let hand_type = match (*counts.iter().max().unwrap_or(&0), jokers) {
     (a,b) if a + b == 5 => 6,
     (a,b) if a + b == 4 => 5,
     (3,0) => if counts.contains(&2) {4} else {3},
@@ -36,18 +42,9 @@ fn get_hand_type(counts: &[usize], jokers: usize) -> usize {
     (1,2) => 3,
     (1,1) => 1,
     _ => 0,
-  }
-}
-
-fn hand_strength(cards: &str, p2: bool) -> (usize, usize) {
-  let counts_by_card = cards.chars().counts();
-  let counts = counts_by_card.iter()
-    .filter(|&(&k,_)| k != 'J' || !p2)
-    .map(|(_,&v)| v)
-    .collect::<Vec<_>>();
-  let jokers = if p2 {*counts_by_card.get(&'J').unwrap_or(&0)} else {0};
-  let idx = cards.chars().fold(0, |acc, c| (acc << 4) + card_index(c, p2));
-  (get_hand_type(&counts, jokers), idx)
+  };
+  let idx = cards.chars().fold(0, |acc, c| (acc << 4) + card_idx(c, p2));
+  (hand_type, idx)
 }
 
 #[aoc::main(07)]
