de7be75214b1c66da6d54027273d781fde11eb1b
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2023-12-30 11:07:02 +0100

    Use simple loops instead of complicated iterators

--- src/bin/09.rs
+++ src/bin/09.rs
@@ -1,24 +1,26 @@
 use itertools::Itertools;
 
-fn solve(xs: Vec<isize>) -> (isize, isize) {
-  let mut v = vec![xs];
-  while v[v.len()-1].iter().any(|&x| x != 0) {
-    let xs = v[v.len()-1].iter()
-      .tuple_windows()
-      .map(|(a,b)| b - a)
-      .collect();
-    v.push(xs);
-  }
-  v.iter().rev().fold((0,0), |(a,b), xs| (xs[xs.len()-1] + a, xs[0] - b))
-}
-
 #[aoc::main(09)]
 fn main(input: &str) -> (isize, isize) {
-  input.split('\n').fold((0,0), |(p1,p2), l| {
+  let (mut p1, mut p2) = (0,0);
+  for l in input.split('\n') {
     let xs = l.split_whitespace()
       .map(|w| w.parse::<isize>().unwrap())
       .collect::<Vec<_>>();
-    let (a, b) = solve(xs);
-    (p1 + a, p2 + b)
-  })
+    let mut v = vec![xs];
+    while v[v.len()-1].iter().any(|&x| x != 0) {
+      let xs = v[v.len()-1].iter()
+        .tuple_windows()
+        .map(|(a,b)| b - a)
+        .collect();
+      v.push(xs);
+    }
+    let mut b = 0;
+    for xs in v.iter().rev() {
+      p1 += xs[xs.len()-1];
+      b = xs[0] - b;
+    }
+    p2 += b;
+  }
+  (p1, p2)
 }
