c6904f333627b2f23b42c93f5c3ffe79760ad96c
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2023-12-31 10:23:37 +0100

    Simplify parsing in 18

--- src/bin/18.rs
+++ src/bin/18.rs
@@ -1,5 +1,5 @@
 fn calc_area(instructions: impl Iterator<Item=(u8, isize)>) -> isize {
-  let (mut a, mut r, mut c) = (0,0,0);
+  let (mut r, mut c, mut a) = (0,0,0);
   for (d, n) in instructions {
     let (rr, cc) = (r,c);
     match d {
@@ -7,8 +7,8 @@ fn calc_area(instructions: impl Iterator<Item=(u8, isize)>) -> isize {
       b'R' => c += n,
       b'D' => r += n,
       b'L' => c -= n,
-      _ => unreachable!()
-    };
+      _ => unreachable!(),
+    }
     a += (c + cc) * (r - rr) + n;
   }
   a / 2 + 1
@@ -18,17 +18,11 @@ fn calc_area(instructions: impl Iterator<Item=(u8, isize)>) -> isize {
 fn main(input: &str) -> (isize, isize) {
   let p1 = input.split('\n').map(|l| {
     let (n, _) = l[2..].split_once(' ').unwrap();
-    (l.as_bytes()[0], n.parse::<isize>().unwrap())
+    (l.as_bytes()[0], n.parse().unwrap())
   });
   let p2 = input.split('\n').map(|l| {
-    let (_, color) = l.split_once(" (#").unwrap();
-    let d = match color.as_bytes()[color.len()-2] {
-      b'0' => b'R',
-      b'1' => b'D',
-      b'2' => b'L',
-      b'3' => b'U',
-      _ => unreachable!(),
-    };
+    let (_, color) = l.split_once('#').unwrap();
+    let d = b"RDLU"[(color.as_bytes()[color.len()-2] - b'0') as usize];
     (d, isize::from_str_radix(&color[0..color.len()-2], 16).unwrap())
   });
   (calc_area(p1), calc_area(p2))
