39a43e166cdb27a5dbe055155ae8657db5464c46
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2024-12-01 09:48:00 +0100

    add my aoc runner and library

---
+++ src/lib.rs
@@ -0,0 +1,29 @@
+use proc_macro::TokenStream;
+use syn::{parse_macro_input, Ident, LitInt, ItemFn};
+
+#[proc_macro_attribute]
+pub fn main(args: TokenStream, input: TokenStream) -> TokenStream {
+  let day = parse_macro_input!(args as LitInt);
+  let input_path = format!("../../inputs/{}.in", day.token());
+
+  let mut aoc_solution = parse_macro_input!(input as ItemFn);
+  aoc_solution.sig.ident = Ident::new("aoc_solution", aoc_solution.sig.ident.span());
+
+  let tokens = quote::quote! {
+    const INPUT: &str = include_str!(#input_path);
+    #aoc_solution
+    fn main() {
+      let now = ::std::time::Instant::now();
+      let (p1, p2) = aoc_solution(INPUT.trim_end());
+      let elapsed = now.elapsed();
+      println!("Part one: {}", p1);
+      println!("Part two: {}", p2);
+      if elapsed.as_millis() > 0 {
+        println!("Time: {}ms", elapsed.as_millis());
+      } else {
+        println!("Time: {}μs", elapsed.as_micros());
+      }
+    }
+  };
+  TokenStream::from(tokens)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+use std::{fs, process::Command, error::Error};
+use itertools::Itertools;
+
+fn extract_microseconds(output: &str) -> Result<usize, Box<dyn Error>> {
+  let out = output.lines().last().unwrap();
+  let time = if out.ends_with("ms") {
+    out["Time: ".len()..out.len()-2].parse::<usize>()? * 1000
+  } else {
+    out["Time: ".len()..out.len()-3].parse::<usize>()?
+  };
+  Ok(time)
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+  let days = fs::read_dir(concat!(env!("CARGO_MANIFEST_DIR"), "/src/bin/"))?
+    .filter_map(|p| p.ok()?.path().file_stem()?.to_str().map(str::to_string))
+    .filter(|day| day.chars().all(|c| c.is_ascii_digit()))
+    .sorted()
+    .collect::<Vec<_>>();
+  let mut total_time = 0;
+  for day in &days {
+    let cmd = Command::new("cargo").args(["run", "--release", "--bin", day]).output()?;
+    if !cmd.status.success() {
+      println!("{}", String::from_utf8(cmd.stderr)?);
+      return Err(format!("Failed to compile day {day}!").into());
+    }
+    let output = String::from_utf8(cmd.stdout)?;
+    println!("Day {}:\n{}", day, output);
+    total_time += extract_microseconds(&output)?;
+  }
+  println!("Total time: {}ms", total_time / 1000);
+  Ok(())
 }
