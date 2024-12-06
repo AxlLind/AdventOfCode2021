33196b38b45188a861d036ac6e4c98029014604c
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2024-12-06 08:27:43 +0100

    06: don't return visited squares for part 2

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -1,6 +1,6 @@
 use itertools::Itertools;
 
-fn walk(m: &[Vec<u8>], mut r: usize, mut c: usize) -> Option<Vec<(usize, usize)>> {
+fn walk(m: &[Vec<u8>], mut r: usize, mut c: usize, return_squares: bool) -> Option<Vec<(usize, usize)>> {
     let mut seen = vec![vec![[false; 4]; m[0].len()]; m.len()];
     let mut d = 0;
     loop {
@@ -11,6 +11,9 @@ fn walk(m: &[Vec<u8>], mut r: usize, mut c: usize) -> Option<Vec<(usize, usize)>
         let (dr, dc) = [(-1,0), (0,1), (1,0), (0, -1)][d];
         let (rr, cc) = (r + dr as usize, c + dc as usize);
         if !(0..m.len()).contains(&rr) || !(0..m[0].len()).contains(&cc) {
+            if !return_squares {
+                return Some(Vec::new());
+            }
             let visited = (0..m.len()).cartesian_product(0..m[0].len())
                 .filter(|&(r, c)| seen[r][c].iter().any(|&b| b))
                 .collect();
@@ -30,12 +33,11 @@ fn main(input: &str) -> (usize, usize) {
     let (sr, sc) = (0..m.len()).cartesian_product(0..m[0].len())
         .find(|&(r, c)| m[r][c] == b'^')
         .unwrap();
-    let p1 = walk(&m, sr, sc).unwrap();
+    let p1 = walk(&m, sr, sc, true).unwrap();
     let p2 = p1.iter().filter(|&&(r, c)| {
-        let saved = m[r][c];
         m[r][c] = b'#';
-        let ok = walk(&m, sr, sc).is_none();
-        m[r][c] = saved;
+        let ok = walk(&m, sr, sc, false).is_none();
+        m[r][c] = b'.';
         ok
     }).count();
     (p1.len(), p2)
