d44ef5cb7e2f654c85aa6b129277889d56210ae3
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2024-12-08 11:51:15 +0100

    08: inline computation

--- src/bin/08.rs
+++ src/bin/08.rs
@@ -1,24 +1,6 @@
 use hashbrown::{HashMap, HashSet};
 use itertools::Itertools;
 
-fn add_node(grid: &[&[u8]], antinodes: &mut HashSet<(usize, usize)>, (r1, c1): (usize, usize), (r2, c2): (usize, usize), part_one: bool) {
-    let (dr, dc) = (r2 - r1, c2 - c1);
-    let mut c = c2 + dc;
-    let mut r = r2 + dr;
-    if part_one {
-        if (0..grid.len()).contains(&r) && (0..grid[0].len()).contains(&c) {
-            antinodes.insert((r, c));
-        }
-    } else {
-        antinodes.insert((r2, c2));
-        while (0..grid.len()).contains(&r) && (0..grid[0].len()).contains(&c) {
-            antinodes.insert((r, c));
-            c += dc;
-            r += dr;
-        }
-    }
-}
-
 #[aoc::main(08)]
 fn main(input: &str) -> (usize, usize) {
     let grid = input.lines().map(|l| l.as_bytes()).collect::<Vec<_>>();
@@ -31,14 +13,25 @@ fn main(input: &str) -> (usize, usize) {
         }
     }
 
-    let mut p1 = HashSet::new();
-    let mut p2 = HashSet::new();
+    let (mut p1, mut p2) = (HashSet::new(), HashSet::new());
     for v in nodes.values() {
         for (&a, &b) in v.iter().tuple_combinations() {
-            add_node(&grid, &mut p1, a, b, true);
-            add_node(&grid, &mut p1, b, a, true);
-            add_node(&grid, &mut p2, a, b, false);
-            add_node(&grid, &mut p2, b, a, false);
+            for ((r1, c1), (r2, c2)) in [(a, b), (b, a)] {
+                let (dr, dc) = (r2 - r1, c2 - c1);
+                let mut c = c2 + dc;
+                let mut r = r2 + dr;
+
+                if (0..grid.len()).contains(&r) && (0..grid[0].len()).contains(&c) {
+                    p1.insert((r, c));
+                }
+
+                p2.insert((r2, c2));
+                while (0..grid.len()).contains(&r) && (0..grid[0].len()).contains(&c) {
+                    p2.insert((r, c));
+                    c += dc;
+                    r += dr;
+                }
+            }
         }
     }
     (p1.len(), p2.len())
