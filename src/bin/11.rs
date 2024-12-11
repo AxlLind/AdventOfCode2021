791450800c3f1ab56e0cd64eaaa78da2127dc0d7
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2024-12-11 06:16:02 +0100

    11: solve both parts

--- src/bin/11.rs
+++ src/bin/11.rs
@@ -1,25 +1,33 @@
 use hashbrown::HashMap;
 
-#[aoc::main(11)]
-fn main(input: &str) -> (usize, usize) {
-    let mut stones = input.split_whitespace().map(|w| (w.parse::<i64>().unwrap(), 1)).collect::<HashMap<i64, usize>>();
-    for _ in 0..75 {
-        let mut new_stones = HashMap::with_capacity(stones.len());
-        for (&s, &v) in &stones {
-            match s {
-                0 => *new_stones.entry(1).or_default() += v,
-                _ => {
-                    let x = s.to_string();
-                    if x.len() % 2 == 0 {
-                        *new_stones.entry(x[0..x.len() / 2].parse().unwrap()).or_default() += v;
-                        *new_stones.entry(x[x.len() / 2..].parse().unwrap()).or_default() += v;
-                    } else {
-                        *new_stones.entry(s * 2024).or_default() += v
-                    }
+fn update(old_stones: &HashMap<i64, usize>) -> HashMap<i64, usize> {
+    let mut stones = HashMap::with_capacity(old_stones.len());
+    for (&s, &v) in old_stones {
+        match s {
+            0 => *stones.entry(1).or_default() += v,
+            _ => {
+                let x = s.to_string();
+                if x.len() % 2 == 0 {
+                    *stones.entry(x[0..x.len() / 2].parse().unwrap()).or_default() += v;
+                    *stones.entry(x[x.len() / 2..].parse().unwrap()).or_default() += v;
+                } else {
+                    *stones.entry(s * 2024).or_default() += v
                 }
             }
         }
-        stones = new_stones;
     }
-    (stones.values().sum(), 0)
+    stones
+}
+
+#[aoc::main(11)]
+fn main(input: &str) -> (usize, usize) {
+    let mut stones = input.split_whitespace().map(|w| (w.parse::<i64>().unwrap(), 1)).collect::<HashMap<i64, usize>>();
+    let mut p1 = 0;
+    for i in 0..75 {
+        stones = update(&stones);
+        if i == 25 {
+            p1 = stones.values().sum();
+        }
+    }
+    (p1, stones.values().sum())
 }
