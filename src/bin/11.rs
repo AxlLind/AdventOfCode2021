33a9d6ae0d03fb269ab7f60ec9bb1bae48efaab8
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2024-12-11 06:27:41 +0100

    11: avoid to_string op

--- src/bin/11.rs
+++ src/bin/11.rs
@@ -6,10 +6,10 @@ fn update(old_stones: &HashMap<i64, usize>) -> HashMap<i64, usize> {
         match s {
             0 => *stones.entry(1).or_default() += v,
             _ => {
-                let x = s.to_string();
-                if x.len() % 2 == 0 {
-                    *stones.entry(x[0..x.len() / 2].parse().unwrap()).or_default() += v;
-                    *stones.entry(x[x.len() / 2..].parse().unwrap()).or_default() += v;
+                let digits = s.ilog10() + 1;
+                if digits % 2 == 0 {
+                    *stones.entry(s % 10i64.pow(digits / 2)).or_default() += v;
+                    *stones.entry(s / 10i64.pow(digits / 2)).or_default() += v;
                 } else {
                     *stones.entry(s * 2024).or_default() += v
                 }
@@ -24,10 +24,10 @@ fn main(input: &str) -> (usize, usize) {
     let mut stones = input.split_whitespace().map(|w| (w.parse::<i64>().unwrap(), 1)).collect::<HashMap<i64, usize>>();
     let mut p1 = 0;
     for i in 0..75 {
-        stones = update(&stones);
         if i == 25 {
             p1 = stones.values().sum();
         }
+        stones = update(&stones);
     }
     (p1, stones.values().sum())
 }
