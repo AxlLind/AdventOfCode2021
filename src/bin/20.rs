780c1f17f1a05b84e49313b3462e8f9d78cce914
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2024-12-20 06:53:13 +0100

    20: compute both parts in the same loop

--- src/bin/20.rs
+++ src/bin/20.rs
@@ -2,21 +2,6 @@ use std::collections::VecDeque;
 use hashbrown::HashMap;
 use itertools::Itertools;
 
-fn count(dists: &HashMap<(usize, usize), usize>, max_step: usize) -> usize {
-    let mut ans = 0;
-    for ((&(r1, c1), &n1), (&(r2, c2), &n2)) in dists.iter().tuple_combinations() {
-        let d = r1.abs_diff(r2) + c1.abs_diff(c2);
-        if d > max_step {
-            continue;
-        }
-        let saved = n2.abs_diff(n1) - d;
-        if saved >= 100 {
-            ans += 1;
-        }
-    }
-    ans
-}
-
 #[aoc::main(20)]
 fn main(input: &str) -> (usize, usize) {
     let g = input.lines().map(str::as_bytes).collect::<Vec<_>>();
@@ -30,7 +15,7 @@ fn main(input: &str) -> (usize, usize) {
             }
         }
     }
-    let mut q = VecDeque::from([(start.0, start.1, 0)]);
+    let mut q = VecDeque::from([(start.0, start.1, 0usize)]);
     let mut dists = HashMap::new();
     while let Some((r, c, n)) = q.pop_front() {
         if dists.contains_key(&(r, c)) {
@@ -42,11 +27,20 @@ fn main(input: &str) -> (usize, usize) {
         }
         for (dr, dc) in [(-1, 0), (1, 0), (0, -1), (0, 1)] {
             let (rr, cc) = (r + dr as usize, c + dc as usize);
-            let x = g[rr][cc];
-            if x != b'#' {
-                q.push_back((rr, cc, n+1));
+            if g[rr][cc] != b'#' {
+                q.push_back((rr, cc, n + 1));
             }
         }
     }
-    (count(&dists, 2), count(&dists, 20))
+    let (mut p1, mut p2) = (0, 0);
+    for ((&(r1, c1), &n1), (&(r2, c2), &n2)) in dists.iter().tuple_combinations() {
+        let d = r1.abs_diff(r2) + c1.abs_diff(c2);
+        if d <= 20 && n2.abs_diff(n1) >= d + 100 {
+            if d <= 2 {
+                p1 += 1;
+            }
+            p2 += 1;
+        }
+    }
+    (p1, p2)
 }
