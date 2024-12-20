e69edce786481664bc98391750ca2498ad1f3f2c
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2024-12-20 06:49:21 +0100

    20: dont calculate distances twice

--- src/bin/20.rs
+++ src/bin/20.rs
@@ -2,25 +2,7 @@ use std::collections::VecDeque;
 use hashbrown::HashMap;
 use itertools::Itertools;
 
-fn bfs(g: &[&[u8]], (sr, sc): (usize, usize), end: (usize, usize), max_step: usize) -> usize {
-    let mut q = VecDeque::from([(sr, sc, 0usize)]);
-    let mut dists = HashMap::new();
-    while let Some((r, c, n)) = q.pop_front() {
-        if dists.contains_key(&(r, c)) {
-            continue;
-        }
-        dists.insert((r, c), n);
-        if (r, c) == end {
-            continue;
-        }
-        for (dr, dc) in [(-1, 0), (1, 0), (0, -1), (0, 1)] {
-            let (rr, cc) = (r + dr as usize, c + dc as usize);
-            let x = g[rr][cc];
-            if x != b'#' {
-                q.push_back((rr, cc, n+1));
-            }
-        }
-    }
+fn count(dists: &HashMap<(usize, usize), usize>, max_step: usize) -> usize {
     let mut ans = 0;
     for ((&(r1, c1), &n1), (&(r2, c2), &n2)) in dists.iter().tuple_combinations() {
         let d = r1.abs_diff(r2) + c1.abs_diff(c2);
@@ -48,7 +30,23 @@ fn main(input: &str) -> (usize, usize) {
             }
         }
     }
-    let p1 = bfs(&g, start, end, 2);
-    let p2 = bfs(&g, start, end, 20);
-    (p1, p2)
+    let mut q = VecDeque::from([(start.0, start.1, 0)]);
+    let mut dists = HashMap::new();
+    while let Some((r, c, n)) = q.pop_front() {
+        if dists.contains_key(&(r, c)) {
+            continue;
+        }
+        dists.insert((r, c), n);
+        if (r, c) == end {
+            continue;
+        }
+        for (dr, dc) in [(-1, 0), (1, 0), (0, -1), (0, 1)] {
+            let (rr, cc) = (r + dr as usize, c + dc as usize);
+            let x = g[rr][cc];
+            if x != b'#' {
+                q.push_back((rr, cc, n+1));
+            }
+        }
+    }
+    (count(&dists, 2), count(&dists, 20))
 }
