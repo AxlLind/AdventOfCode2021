5928a1f97769d4e66ce0206839d3b31a5d92974b
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2024-12-22 08:10:27 +0100

    22: 7x speedup by smaller hashmap key

--- src/bin/22.rs
+++ src/bin/22.rs
@@ -1,29 +1,28 @@
 use hashbrown::{HashMap, HashSet};
 use itertools::Itertools;
 
-fn prices(mut p: i64) -> Vec<i64> {
-    let mut ps = vec![p];
-    for _ in 0..2000 {
-        p = (p ^ (p * 64))   % 16777216;
-        p = (p ^ (p / 32))   % 16777216;
-        p = (p ^ (p * 2048)) % 16777216;
-        ps.push(p);
-    }
-    ps
-}
-
 #[aoc::main(22)]
 fn main(input: &str) -> (i64, i64) {
     let (mut p1, mut p2) = (0, HashMap::new());
+    let mut seen = HashSet::new();
     for l in input.lines() {
-        let mut ps = prices(l.parse().unwrap());
-        p1 += ps.last().unwrap();
+        let mut ps = [0; 2001];
+        let mut p = l.parse().unwrap();
+        ps[0] = p;
+        for i in 0..2000 {
+            p = (p ^ (p * 64))   % 16777216;
+            p = (p ^ (p / 32))   % 16777216;
+            p = (p ^ (p * 2048)) % 16777216;
+            ps[i + 1] = p;
+        }
+        p1 += p;
+
         for p in &mut ps {
             *p %= 10;
         }
-        let mut seen = HashSet::new();
+        seen.clear();
         for (a, b, c, d, e) in ps.iter().tuple_windows() {
-            let k = (b - a, c - b, d - c, e - d);
+            let k = (b - a) + (c - b) * 100 + (d - c) * 10000 + (e - d) * 1000000;
             if seen.insert(k) {
                 *p2.entry(k).or_default() += *e;
             }
