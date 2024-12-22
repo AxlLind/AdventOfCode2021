05bb26b2a56e5bbbeebcfb574342b323a88d7beb
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2024-12-22 14:12:56 +0100

    22: simplify loop

--- src/bin/22.rs
+++ src/bin/22.rs
@@ -6,23 +6,19 @@ fn main(input: &str) -> (i64, i64) {
     let (mut p1, mut p2) = (0, HashMap::new());
     let mut seen = HashSet::new();
     for l in input.lines() {
-        let mut ps = [0; 2001];
-        let mut p = l.parse().unwrap();
-        ps[0] = p;
+        let mut ps = [0; 2000];
+        let mut p = l.parse::<i64>().unwrap();
         for i in 0..2000 {
             p = (p ^ (p * 64))   % 16777216;
             p = (p ^ (p / 32))   % 16777216;
             p = (p ^ (p * 2048)) % 16777216;
-            ps[i + 1] = p;
+            ps[i] = p % 10;
         }
         p1 += p;
 
-        for p in &mut ps {
-            *p %= 10;
-        }
         seen.clear();
         for (a, b, c, d, e) in ps.iter().tuple_windows() {
-            let k = (b - a) + (c - b) * 100 + (d - c) * 10000 + (e - d) * 1000000;
+            let k = (b - a) + (c - b) * 20 + (d - c) * 400 + (e - d) * 8000;
             if seen.insert(k) {
                 *p2.entry(k).or_default() += *e;
             }
