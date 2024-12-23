d72cef26d6130a380eb97e74dd3314ee34a85f02
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2024-12-23 10:58:28 +0100

    merge fetch.sh scripts

--- fetch.sh
+++ fetch.sh
@@ -2,14 +2,13 @@
 set -euo pipefail
 SCRIPT_DIR=$(realpath "$(dirname "$0")")
 
-if [[ $# != 1 ]]; then
-  echo "Please provide a day number."
-  echo "usage: $0 DAY"
+if [[ $# != 2 ]]; then
+  echo "usage: $0 YEAR DAY"
   exit 1
 fi
 
-if [[ ! "$1" =~ ^(0[1-9]|1[0-9]|2[0-5])$ ]]; then
-  echo "Not a valid day: $1"
+if [[ ! "$2" =~ ^(0[1-9]|1[0-9]|2[0-5])$ ]]; then
+  echo "Not a valid day: $2"
   exit 1
 fi
 
@@ -21,10 +20,10 @@ fi
 TMPFILE=$(mktemp)
 trap 'rm -f "$TMPFILE"' EXIT
 
-curl "https://adventofcode.com/2024/day/${1#0}/input"          \
+curl "https://adventofcode.com/$1/day/${2#0}/input"            \
   -s --fail --cookie "session=$AOC_SESSION"                    \
   -A "Bash script at $(git remote -v | awk 'NR==1{print $2}')" \
   | tee "$TMPFILE"
 
-mkdir -p "$SCRIPT_DIR/inputs"
-mv "$TMPFILE" "$SCRIPT_DIR/inputs/$1.in"
+mkdir -p "$SCRIPT_DIR/$1/inputs"
+mv "$TMPFILE" "$SCRIPT_DIR/$1/inputs/$2.in"
